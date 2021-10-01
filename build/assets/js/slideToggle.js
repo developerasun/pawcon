e902876b6f6daacfc09fe0c9859158d308e2495f
designerasun <asuntheawesome@gmail.com>
2021-10-01 22:36:10 +0900

    adjusting css display property in js

--- build/assets/js/slideToggle.js
+++ build/assets/js/slideToggle.js
@@ -1,13 +1,27 @@
 
 const trigger = document.querySelector("#trigger");
-const menus = document.querySelectorAll(".navbar-item"); 
+const navbar = document.querySelector(".navbar");
+const navbar_id = document.getElementById("navbar");
 
-function triggerClicked(event) { 
-    event.preventDefault(); 
-    
-    for(i=0; i < menus.length; i++){
-        menus[i].classList.add("hidden"); 
+function triggerOn(event) { 
+    event.preventDefault();
+
+    const testing = document.getElementById("navbar"); 
+
+    if (testing.style.display == "none" & window.innerWidth < 720) {
+        testing.style.display = "block";
+    } else if (testing.style.display == "none" & window.innerWidth >= 720) {
+        testing.style.display = "flex";
+    } else {
+        testing.style.display = "none";
     }
+    
 }
 
-trigger.addEventListener("click", triggerClicked);
+trigger.addEventListener("click", triggerOn);
+
+// Disappearing the navigation items
+// const menus = document.querySelectorAll(".navbar-item"); 
+// for(i=0; i < menus.length; i++){
+//     menus[i].classList.add("active"); 
+// }
