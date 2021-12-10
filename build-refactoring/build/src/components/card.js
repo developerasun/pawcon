8b33e52268dad9f7309cc7a031cf97b9121308ec
designerasun <asuntheawesome@gmail.com>
2021-12-10 18:24:55 +0900

    props added

--- build-refactoring/build/src/components/card.js
+++ build-refactoring/build/src/components/card.js
@@ -1,11 +1,9 @@
 import React from "react";
-import images from "../img";
 
-function Card() { 
-    const image = images.a;
-    const date = new Date(); 
+// props : images, title, author, date
+function Card(props) { 
     return (
-        <div style={{display:'inline-block', margin:10}}>
+        <div className="card-container" style={{display:'inline-block', margin:10}}>
             <div className="card" style={
                 {
                     display : 'flex', 
@@ -18,10 +16,10 @@ function Card() {
                     gap: 10
                 }
             }>
-                <img src={image} alt="card" style={{width:100, height:100}}/>
-                <div className="title">Hello React Card</div>
-                <div className="author">Jake Sung</div>
-                <div className="date">{date.toLocaleDateString()}</div>
+                <img src={props.image} alt="card" style={{width:100, height:100}}/>
+                <div className="title">{props.title}</div>
+                <div className="author">{props.author}</div>
+                <div className="date">{props.date}</div>
             </div>
         </div>
      ); 
