5105958fd6616dcf77aa18d9f76bc15e147d8f59
designerasun <asuntheawesome@gmail.com>
2021-12-10 18:24:24 +0900

    gallery component added

--- build-refactoring/build/src/App.js
+++ build-refactoring/build/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import BestCanvas from './components/bestCanvas';
+import Gallery from './components/gallery';
+// import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BestCanvas />
+      <Gallery galleryName={"Hall Of Fame This Week"}/>
+      {/* <Pagination /> */}
     </div>
   );
 }
