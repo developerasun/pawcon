9e10bbd8141fda123d0c3a230dc4ed55bfe60c9d
designerasun <asuntheawesome@gmail.com>
2021-12-13 16:36:15 +0900

    page 404 component added

---
+++ build-refactoring/build/src/components/notFound.js
@@ -0,0 +1,10 @@
+const NotFound = () => {
+    return (
+        <div className="page404">
+            <h2>Page 404</h2> 
+            <p>This page doesn't exist</p>
+        </div>
+    )
+}
+
+export default NotFound
