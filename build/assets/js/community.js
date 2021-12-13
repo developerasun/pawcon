8fc1e051f61749d968683134c489c09b75b9c81e
designerasun <asuntheawesome@gmail.com>
2021-12-13 23:50:20 +0900

    editor moved to another dir

--- build/assets/js/community.js
+++ build/assets/js/community.js
@@ -1,35 +1,3 @@
-import EditorJs from '@editorjs/editorjs'; 
-
-const Header = require('@editorjs/header');
-const SimpleImage = require('@editorjs/simple-image');
-const submitBtn = document.querySelector(".submitBtn"); 
-
-// declare a new editor from Editor.js
-const editor = new EditorJs( {
-    holder: 'editorjs', 
-    tools: {
-        image : SimpleImage,
-        header : {
-            class : Header, 
-            shortcut: 'CONTROL+H',
-            config : {
-                placeholder : 'Enter a header',
-                levels : [2,3,4],
-                defaultLevel : 2
-            }
-        }
-    }
-});
-
-// connect backend & database later to save a post 
-submitBtn.addEventListener("click", function(){
-    editor.save().then((result)=>{
-        console.log([...result.blocks]);
-    }).catch((err)=>{
-        console.log(err)
-    })
-});
-
 // table CRUD
 const getBtn = document.querySelector(".getBtn")
 let count = 0
