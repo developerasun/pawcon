a41596e9157ae842699d306e0c92f75cccd531e1
designerasun <asuntheawesome@gmail.com>
2021-12-13 16:37:23 +0900

    Router, Route, and Switch component added

--- build-refactoring/build/src/App.js
+++ build-refactoring/build/src/App.js
@@ -1,15 +1,28 @@
 import './App.css';
 import BestCanvas from './components/bestCanvas';
 import Gallery from './components/gallery';
+import Home from './components/home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NotFound from './components/notFound';
 // import Footer from './components/footer';
 
 function App() {
   return (
-    <div className="App">
-      <BestCanvas />
-      <Gallery galleryName={"Hall Of Fame This Week"}/>
-      {/* <Pagination /> */}
-    </div>
+    <Router>
+      <div className="App">
+        <BestCanvas />
+        <Gallery galleryName={"Hall Of Fame This Week"}/>
+        {/* <Pagination /> */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="*">
+            <NotFound/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
