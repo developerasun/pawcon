2ac6e77fcb45f6b7e329a27e39892ae11b2e2b95
designerasun <asuntheawesome@gmail.com>
2021-12-15 01:04:50 +0900

    dummy document created and saved at db

--- build/assets/js/database/connect.js
+++ build/assets/js/database/connect.js
@@ -1,10 +1,21 @@
-const CONFIG = require('./config/key')
-const mongoose = require('mongoose');
-const Post = require('./model');
+// Database URI configuration : 1) .env 2) module.exports
+// const CONFIG = require('./config/key')
+require('dotenv').config()
+const mongoose = require('mongoose')
+const MONGO_URI = process.env.MONGO_URI
+const Post = require('./model')
 
 // connect mongoDB
-mongoose.connect( CONFIG.MONGO_URI, {})
+mongoose.connect( MONGO_URI, {})
         .then(()=>{ console.log("connected") })
         .catch((err)=>{ console.log(err)} )
 
-// const post = new Post()
+// Create a document and save
+const dummyPost = new Post({
+        title: "saving dummies in MongoDB", 
+        author: "Jake Sung",
+        date : new Date().toLocaleDateString()
+})
+
+dummyPost.save().then(()=>{console.log("saving sucess")})
+                .catch((err)=>{console.log(err)})
