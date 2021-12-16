3c37bcf943e543d84dba3d8f48a62a958978bf28
designerasun <asuntheawesome@gmail.com>
2021-12-16 17:16:46 +0900

    home components organized, route paramter set

--- build-refactoring/build/src/App.js
+++ build-refactoring/build/src/App.js
@@ -4,6 +4,7 @@ import Gallery from './components/gallery';
 import Home from './components/home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NotFound from './components/notFound';
+import Details from './components/details';
 // import Footer from './components/footer';
 
 function App() {
@@ -13,13 +14,17 @@ function App() {
         <Switch>
             <Route exact path="/">
               <Home />
+              <BestCanvas />
+              <Gallery galleryName={"Hall Of Fame This Week"}/>
+            </Route>
+            <Route path="/details/:title">
+              <Details />
             </Route>
             <Route path="*">
               <NotFound/>
             </Route>
         </Switch>
-        <BestCanvas />
-        <Gallery galleryName={"Hall Of Fame This Week"}/>
+        
           {/* <Pagination /> */}
       </div>
     </Router>
