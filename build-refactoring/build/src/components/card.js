8609c5e0d60e4210b26df5bbae080bb082122ac4
designerasun <asuntheawesome@gmail.com>
2021-12-16 17:14:48 +0900

    link component with route paramter added

--- build-refactoring/build/src/components/card.js
+++ build-refactoring/build/src/components/card.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 // props : images, title, author, date
 function Card(props) { 
     return (
-        <div className="card-container" style={{display:'inline-block', margin:10}}>
+        <div className="card-container" style={{display:'inline-block', margin:10 }}>
             <div className="card" style={
                 {
                     display : 'flex', 
@@ -13,13 +14,16 @@ function Card(props) {
                     border : "2px solid green", 
                     width:'80%', 
                     padding: '1rem',
-                    gap: 10
+                    gap: 10                
                 }
             }>
                 <img src={props.image} alt="card" style={{width:100, height:100}}/>
                 <div className="title">{props.title}</div>
                 <div className="author">{props.author}</div>
                 <div className="date">{props.date}</div>
+                <Link to={`/details/${props.routeDetails}`}
+                      style={{textDecoration:'none', color:'green', fontWeight:'bold'}}
+                >See detail</Link>
             </div>
         </div>
      ); 
