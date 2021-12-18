8839132f7c2e21e5907c98b854d86eb0752edfc6
designerasun <asuntheawesome@gmail.com>
2021-12-19 00:56:32 +0900

    change : element will show based on scroll now

--- build/assets/js/onScroll.js
+++ build/assets/js/onScroll.js
@@ -2,39 +2,27 @@
 const endCard = document.querySelector(".end-about-developer");
 const careers = document.querySelectorAll(".career"); 
 
-console.log(endCard.getBoundingClientRect());
-console.log(endCard.top);
-console.log(careers[1], careers[2], careers[3]); 
-
-// testing.addEventListener("click", (event)=>{
-//     event.preventDefault();
-// })
-
-// card shown on scroll
-window.addEventListener("scroll", ()=>{
-    let scrollY = window.scrollY;
-    
-    if (scrollY > careers[1].getBoundingClientRect().top) {
-            careers[1].style.display = "block";
-            careers[1].classList.add("active");
-        }
-    if (scrollY > careers[2].getBoundingClientRect().top) {
-            setTimeout(()=> { 
-                careers[2].style.display = "block";
-                careers[2].classList.add("active");
-            }, 500)
-    }
-    if (scrollY > careers[3].getBoundingClientRect().top) {
-            setTimeout(()=>{
-                careers[3].style.display = "block";
-                careers[3].classList.add("active");
-            },1000)
-    }
-})
-
 // card not shown when load
 window.addEventListener("load", ()=>{
     for (let i=1; i<careers.length; i++) {
         careers[i].style.display = "none";
     }
 })
+
+// card shown depending on scroll Y moving
+window.addEventListener("scroll", ()=>{
+    if (window.scrollY > 500) {
+        showNthCareers(1)
+    }
+    if (window.scrollY > 800) {
+        showNthCareers(2)
+    }
+    if (window.scrollY > 1100) {
+        showNthCareers(3)
+    }
+})
+
+const showNthCareers = (nthItem) => {
+        careers[nthItem].style.display = "block"
+        careers[nthItem].classList.add("active")
+}
