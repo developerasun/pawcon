88162627f8522ace032778c16bff39af30f92ba3
designerasun <asuntheawesome@gmail.com>
2021-12-29 21:42:32 +0900

    get and display editor.js object in html

--- build/assets/js/editor/editor.js
+++ build/assets/js/editor/editor.js
@@ -1,8 +1,9 @@
 const EditorJs = require('@editorjs/editorjs')
 const Header = require('@editorjs/header')
 const SimpleImage = require('@editorjs/simple-image')
-
+const editorDisplay = document.querySelector(".editorDisplay")
 const submitBtn = document.querySelector(".submitBtn")
+
 // declare a new editor from Editor.js
 const editor = new EditorJs( {
     holder: 'editorjs', 
@@ -20,11 +21,18 @@ const editor = new EditorJs( {
     }
 });
 
-// connect backend & database later to save a post 
-submitBtn.addEventListener("click", function(){
-    editor.save().then((result)=>{
-        console.log([...result.blocks]);
-    }).catch((err)=>{
-        console.log(err)
-    })
-});
+// Below codes not tested yet : 2021.12.29 written
+// button clicked => start post saving process
+submitBtn.addEventListener("click", ()=>{
+    editor.save() // returns an editor.js post object( per paragraph/block) : data.blocks[i].data.text
+          .then((result)=> {
+            // iterate blocks array and save as one array
+            const allParagraphs = result.blocks.map((item, idx) => item[idx].data.text )
+
+            // Join the array to make them one text
+            const oneEditorPost = allParagraphs.join(" ")
+
+            // Display the text in HTML
+             editorDisplay.innerText += oneEditorPost
+          });
+})
