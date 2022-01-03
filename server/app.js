dfa66a96581d768afe742b694c602f78f17b4002
designerasun <asuntheawesome@gmail.com>
2022-01-04 00:21:10 +0900

    request logger middleware added, db connected

--- server/app.js
+++ server/app.js
@@ -1,16 +1,43 @@
-// const express = require('express')
-// const app = express()
-
 require('dotenv').config()
-const mongoose = require('mongoose')
+
+const express = require('express')
+const app = express()
+const path = require('path')
+const router = require('./router/route')
 const MONGO_URI = process.env.MONGO_URI
+const PORT = process.env.PORT || 8080
+const mongoose = require('mongoose')
 
 // Connect mongoDB 
 mongoose.connect( MONGO_URI, {})
         .then(()=>{ 
                 console.log("db connected") 
                 // and then start Express app
-                // app.listen(PORT, ()=> console.log(`app executed at ${PORT}`))
+                app.listen(PORT, ()=> console.log(`app executed at ${PORT}`))
         })
         .catch((err)=>{ console.log(err)} )
+
+// setting middlewares
+app.use((req, res, next)=> {
+        console.group('<<<<< logging requests >>>>>')
+        console.log(
+                `method : ${req.method},
+                 url : ${req.url}, 
+                 host name : ${req.hostname}
+                 path : ${req.path}`)
+        console.groupEnd()
+        next()
+})
+app.use(router)
+
+
+// app.use(express.static(path.join(__dirname, '../client/build', 'index.html')))
+// app.use(express.static('public'))
+// app.use((req, res, next)=> {
+//         res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
+// })
+
+app.get('/', (req, res)=> {
+        res.send("hello server")
+})
 
