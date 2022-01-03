c5ca666e75101704dfc5f5c2e9bd4918b0f74594
designerasun <asuntheawesome@gmail.com>
2022-01-04 00:18:41 +0900

    login, signup controller added

---
+++ server/controller/controller.js
@@ -0,0 +1,14 @@
+// login controller
+const login_get = () => {}
+const login_post = () => {}
+
+// sign up controller
+const signup_get = () => {}
+const signup_post = () => {}
+
+module.exports = {
+    login_get,
+    login_post,
+    signup_get,
+    signup_post
+}
