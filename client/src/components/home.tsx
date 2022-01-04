148edad873967cb94e0b7855d47c56bd6cc55804
designerasun <asuntheawesome@gmail.com>
2022-01-05 00:58:06 +0900

    counterContainer added but failed to implement

--- client/src/components/home.tsx
+++ client/src/components/home.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Navbar } from './partials/nav';
 
 export interface HomeProps {
 }
@@ -6,7 +7,14 @@ export interface HomeProps {
 export function Home (props:  HomeProps) {
   return (
     <div>
+      <Navbar />
       <h1>home route</h1>
+
+      <section className="counterContainer">
+          <p className='count'>first count here</p>
+          <p className='count'>second count here</p>
+          <p className='count'>third count here</p>
+      </section>
     </div>
   );
 }
