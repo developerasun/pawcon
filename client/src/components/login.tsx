35c7717132da1b3bca1325736d4094c66cdb4d23
designerasun <asuntheawesome@gmail.com>
2022-01-06 10:41:39 +0900

    conditional rendering with useState hook

--- client/src/components/login.tsx
+++ client/src/components/login.tsx
@@ -1,25 +1,33 @@
 import * as React from 'react';
+import { ProfileProps } from './containers/propContatiner';
 import { LoginForm } from './partials/loginPartials/loginForm'
 import { Profile } from './partials/loginPartials/profile'
 import { Navbar } from './partials/nav';
 
-// export interface ILoginProps {
-//   loginStatus : boolean
-//   component : React.ComponentType
-// }
-
 // conditional rendering based on login status
-export function Login () {
-  const [loginStatus, setLoginStatus] = React.useState(false)
-  const handleLogin = () => setLoginStatus(true)
-  const handleLogout = () => setLoginStatus(false)
+export function Login() {
+  const [user, setUser] = React.useState<ProfileProps | null>(null)
+  
+  // handleLogin => fetch(GET) user info 
+  // handleLogout => reset form  user info
+  const handleLogin = () => {
+    setUser({name : "Jake", email: "Jake@gmail.com", isLoggedIn:true})
+  }
+  const handleLogout = () => setUser(null)
 
   return (
     <div>
       <Navbar />
+
+      {/* if user exists(logged in), render Profile component */}
+      {/* if not(logged out / null), render LoginForm component */}
       <button onClick={handleLogin}>Login</button>
       <button onClick={handleLogout}>Logout</button>
-      {loginStatus===true ? <Profile /> : <LoginForm />}
+      {user?.isLoggedIn===true ? <Profile 
+                                    name={user.name}
+                                    email={user.email}
+                                    isLoggedIn={user.isLoggedIn}/> 
+                                : <LoginForm />}
     </div>
   );
 }
