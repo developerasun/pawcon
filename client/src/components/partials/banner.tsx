911b7f6da44f15a575073baa8a48a4529149d290
designerasun <asuntheawesome@gmail.com>
2022-01-06 10:43:32 +0900

    email subsriber, img banner components created

--- client/src/components/partials/banner.tsx
+++ client/src/components/partials/banner.tsx
@@ -1,12 +1,35 @@
 import * as React from 'react';
+import { ImgBannerStyle } from '../containers/styleContainer'
 
-export interface IBannerProps {
+export type BannerProps = {
+  title : string
 }
 
-export function Banner (props: IBannerProps) {
+export function EmailSubscribeBanner ( { title }: BannerProps) {
+  const handleSubmit = () => { 
+
+  }
   return (
-    <div>
-      
-    </div>
+    <article>
+      <h3>Join {title} Today!</h3>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, animi?</p>
+
+      <form onSubmit={handleSubmit}>
+          <input type="email" name="" id="" placeholder="enter your email" required/>
+          <button>Submit</button>
+      </form>
+    </article>
   );
+}
+
+export type ImgBannerProps = {
+  src : string
+}
+
+export function ImgBanner ({ src } : ImgBannerProps) { 
+  return ( 
+    <article>
+      <img src={src} alt="image banner" style={ImgBannerStyle}/>
+    </article>
+  )
 }
