094a0faad103461dbadbe674dfd2a2d2f66ce2cd
designerasun <asuntheawesome@gmail.com>
2022-01-06 10:45:22 +0900

    temp api component set

---
+++ client/src/components/apis/temp.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+export const Temp = () => {
+    // React.useEffect(()=>{
+    //     fetch("https://jsonplaceholder.typicode.com/users/1")
+    //     .then((response)=> response.json())
+    //     .then((data)=> { 
+    //       console.log(data)
+    //       setUser({ name: data.name, email: data.email, isLoggedIn : true})
+    //     })
+    //     .catch((err)=> console.log(err))
+    //   }, [])
+}
