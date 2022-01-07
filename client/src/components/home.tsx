5012178fbab22be81944c42e575426e0e13b6a77
designerasun <asuntheawesome@gmail.com>
2022-01-07 23:24:50 +0900

    counter added

--- client/src/components/home.tsx
+++ client/src/components/home.tsx
@@ -2,19 +2,37 @@ import * as React from 'react';
 import { Navbar } from './partials/nav';
 import { ImgBanner } from './partials/banner'
 import { tempImg } from './gallery'
+import { IncreaseToTarget } from './partials/homePartials/counter'
+
 export interface HomeProps {
 }
 
 export function Home (props:  HomeProps) {
+  const abortController = new AbortController()
+  // Import counter and use it in useEffect hook. 
+  // The useEffect is used to 1) fetching data 2) manipulating DOM in React.
+  React.useEffect(()=> {
+    const counters = document.querySelectorAll(".counter") as NodeListOf<HTMLParagraphElement>
+    console.log(counters[0])
+
+    IncreaseToTarget( { target : 101, count : 0, speed : 10, display: counters[0]})
+    IncreaseToTarget( { target : 501, count : 0, speed : 10, display: counters[1]})
+    IncreaseToTarget( { target : 251, count : 0, speed : 10, display: counters[2]})
+
+    // useEffect clean upfunction here
+    return () => abortController.abort()
+  }, [])
+
   return (
     <div>
       <Navbar />
       <h1>home route</h1>
 
+      {/* Counter component here */}
       <section className="counterContainer">
-          <p className='count'>first count here</p>
-          <p className='count'>second count here</p>
-          <p className='count'>third count here</p>
+          <p className='counter'></p>
+          <p className='counter'></p>
+          <p className='counter'></p>
       </section>
 
       <ImgBanner src={tempImg}/>
