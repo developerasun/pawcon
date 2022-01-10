518e2d3d2908f00e7a1f2a723ab147c1595508b6
designerasun <asuntheawesome@gmail.com>
2022-01-11 00:19:19 +0900

    login flow explained, React-Node js testing, Linked to SignUp component

--- client/src/components/partials/loginPartials/loginForm.tsx
+++ client/src/components/partials/loginPartials/loginForm.tsx
@@ -1,16 +1,51 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
+
+// login form flow for React Router
+// 1) Login get
+// 2) Logout get
+// 3) Signup get
 
 export interface  LoginFormProps {
 }
 
 export function LoginForm (props:  LoginFormProps) {
-  const handleSubmit = () => {
-
+  // const [submit, setSubmit] = React.useState(false)
+  const [click, setClick] = React.useState(false)
+  
+  const handleSubmit = (event : React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    setClick(true)
+    console.log(click)
   }
+
+  // TEST :React to Node Js testing
+  React.useEffect(()=> {
+    fetch('/login').then((res)=> { res.json() })
+    // FIX : something wrong here => data logged undefined
+                   .then((data) => console.log(data))
+                   .catch((err) => console.log(err))
+  }, [click])
+
+  // // User sign up logic
+  // React.useEffect(()=> {
+  //   const email = document.querySelector("#email") as HTMLInputElement
+  //   const password = document.querySelector("#password") as HTMLInputElement
+  //   fetch('/signup', {
+  //     method: 'POST', 
+  //     body: JSON.stringify({
+  //       email : email, 
+  //       password : password
+  //     }), 
+  //     headers : { 'Content-Type' : 'application/json' }
+  //   })
+  // }, [ ]) // add dependency here
+
   return (
     <div>
       <h2>Join PawCon today!</h2>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, animi?</p>
+      <p>Be the one of the coolest NFT art maker in seconds</p>
+      <Link to={`/signup`}>LINK : Not signed up yet?</Link>
       <form onSubmit={handleSubmit}>
           <label htmlFor="email">
             <input 
@@ -26,8 +61,8 @@ export function LoginForm (props:  LoginFormProps) {
                 id='password'      
                 placeholder='Enter your password'
                 required />
-         </label>
-         <button>Login</button>
+         </label> <br />
+         <button type='submit'>Login</button>
       </form>
     </div>
   );
