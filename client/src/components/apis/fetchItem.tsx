46987240515b174a47a6972e4425d8f51deaba2e
designerasun <asuntheawesome@gmail.com>
2022-01-12 00:47:15 +0900

    comment for async added

--- client/src/components/apis/fetchItem.tsx
+++ client/src/components/apis/fetchItem.tsx
@@ -3,5 +3,5 @@ import * as React from 'react';
 export async function fetchItem ( url :  string, abortController: AbortController) {
   const response = await fetch(url)
   const data = await response.json() 
-  return data 
+  return data // return Promise
 }
