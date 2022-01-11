9a4b6b69d98719200a3aaca21cd278725dd6dc14
designerasun <asuntheawesome@gmail.com>
2022-01-12 00:58:13 +0900

    empty sfc added

--- client/src/components/about.tsx
+++ client/src/components/about.tsx
@@ -1,5 +1,11 @@
 import * as React from 'react';
+import { AboutCardsProps } from './containers/propContatiner';
+import { Footer } from './partials/footer';
 import { Navbar } from './partials/nav';
+
+const AboutCards = ({ date, title, description, image }: AboutCardsProps) => {
+
+}
 
 export interface IAboutProps {
 }
@@ -8,7 +14,13 @@ export function About (props: IAboutProps) {
   return (
     <div>
       <Navbar />
-      <h1>about page</h1>
+      <main>
+        <h1>About PawCon and Jake Sung</h1> 
+        <h2>About PawCon</h2> 
+        <h2>About Jake Sung</h2> 
+        
+      </main>
+      <Footer />
     </div>
   );
 }
