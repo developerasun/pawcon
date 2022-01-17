468349508dbd876b5b99d78f550b10449a074a69
designerasun <asuntheawesome@gmail.com>
2022-01-18 00:27:50 +0900

    minor changes

--- client/src/components/_about/about.tsx
+++ client/src/components/_about/about.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { AboutCardsProps } from '../containers/propContatiner';
 import { Footer } from '../subComponents/footer';
 import { Navbar } from '../subComponents/navbar';
+import './sass/css/about.css'
 
 const AboutCards = ({ date, title, description, image }: AboutCardsProps) => {
 
@@ -12,15 +13,11 @@ export interface IAboutProps {
 
 export function About (props: IAboutProps) {
   return (
-    <div>
-      <Navbar />
       <main>
         <h1>About PawCon and Jake Sung</h1> 
         <h2>About PawCon</h2> 
         <h2>About Jake Sung</h2> 
-        
+        <div className='test'>sass test</div>
       </main>
-      <Footer />
-    </div>
   );
 }
