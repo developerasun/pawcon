72d29691f61323a3c4b9b232672ad0dee0f0109f
designerasun <asuntheawesome@gmail.com>
2022-01-20 21:54:02 +0900

    switchable tab comp created

--- client/src/components/_community/community.tsx
+++ client/src/components/_community/community.tsx
@@ -1,17 +1,38 @@
 import * as React from 'react';
-import { Navbar } from '../subComponents/navbar';
-import { BlogList } from './blogList';
-import { BlogForm } from './blogForm';
+import './sass/css/community.css';
 
-export interface  CommunityProps {
-}
-
-export function Community (props:  CommunityProps) {
+// MenuTabs should take component as argument 
+// this component will be imported and used in '/create' as well. 
+export const MenuTabs = () => {
+  const [visibility, setVisibility] = React.useState(true)
+  const handleClick = () => setVisibility(!visibility)
+  
   return (
     <div>
+      <span>create switchable tabs based on click</span>
+
+        <div 
+            className={visibility ? `visible` : 'hidden' }
+            onClick={handleClick} 
+            style={{"backgroundColor" : "pink"}}>
+          <span>component 1 here</span>
+        </div>
+
+        <li style={{"backgroundColor" : "lightblue"}}>component 2 here</li>
+        <li style={{"backgroundColor" : "tomato"}}>component 3 here</li>
+
+    </div>
+  )
+}
+
+export function Community () {
+  return (
+    <div>
+      
       <h1>community route</h1>
-      <BlogList />
-      <BlogForm />
+      {/* component args : 1) blog 2) chat 3) feedback */}
+      <MenuTabs />
+
     </div>
   );
 }
