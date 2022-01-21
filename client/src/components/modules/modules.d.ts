d612b0cffe59e1a258ae6d6d4dad16bce3523b11
designerasun <asuntheawesome@gmail.com>
2022-01-21 18:16:17 +0900

    declare editor.js header module

---
+++ client/src/components/modules/modules.d.ts
@@ -0,0 +1,10 @@
+// If you use editor.js in TypeScript you need to explicitly specify 
+// that typeof Tool implements BlockToolConstructable or InlineToolConstructable
+declare module '@editorjs/header' {
+    import { BlockTool } from '@editorjs/editorjs'
+
+    export default class EditorJSHeader implements BlockTool {
+        save(block: HTMLElement);
+        render(): HTMLElement;
+    }
+}
