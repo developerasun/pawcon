9c7ed3b684e9f78916d7bc628c96fb7b5b16ec23
designerasun <asuntheawesome@gmail.com>
2022-01-21 20:41:33 +0900

    MenuTab props, table, dummy comp added

--- client/src/components/_community/community.tsx
+++ client/src/components/_community/community.tsx
@@ -2,29 +2,50 @@ import * as React from 'react';
 import { Feedback } from './feedback';
 import './sass/css/community.css';
 
+interface MenuTabsProps {
+  // MenuTabs props : a component or Array of components 
+  component : React.ComponentProps<React.ComponentType> | React.ComponentProps<React.ComponentType>[]
+}
+
 // MenuTabs should take component as argument 
 // this component will be imported and used in '/create' as well. 
-export const MenuTabs = () => {
+export const MenuTabs = ( { component } : MenuTabsProps ) => {
   const [visibility, setVisibility] = React.useState(true)
+
+  React.useEffect(()=> {
+    
+  }, [ ])
   const handleClick = () => setVisibility(!visibility)
   
   return (
     <div>
       <span>create switchable tabs based on click</span>
 
-        <div 
-            className={visibility ? `visible` : 'hidden' }
-            onClick={handleClick} 
-            style={{"backgroundColor" : "pink"}}>
-          <span>component 1 here</span>
-        </div>
+      <table>
+        <thead>
+          <tr>
+            <th onClick={handleClick} >Component 111</th>
+            <th >Component 222</th>
+            <th >Component 333</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className={visibility ? `visible` : 'hidden' } style={{"backgroundColor" : "pink"}}>
+              {component}
+            </td>
+            <td className={visibility ? `visible` : 'hidden' } style={{"backgroundColor" : "lightblue"}}>ss</td>
+            <td className={visibility ? `visible` : 'hidden' } style={{"backgroundColor" : "tomato"}}>ff</td>
+          </tr>
+        </tbody>
+      </table>
 
-        <li style={{"backgroundColor" : "lightblue"}}>component 2 here</li>
-        <li style={{"backgroundColor" : "tomato"}}>component 3 here</li>
-
-   
     </div>
   )
+}
+
+const Dummy = () => {
+  return <>hello I'm dummy component</>
 }
 
 export function Community () {
@@ -33,7 +54,7 @@ export function Community () {
       
       <h1>community route</h1>
       {/* component args : 1) blog 2) chat 3) feedback */}
-      <MenuTabs />
+      <MenuTabs component={<Dummy />}/>
       editor js here
       {/* <Feedback /> */}
     </div>
