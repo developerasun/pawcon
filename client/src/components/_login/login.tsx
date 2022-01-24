1c8b59674a8291739c98b332cf942385f9e0cb15
designerasun <asuntheawesome@gmail.com>
2022-01-24 23:12:03 +0900

    feat : conditional render with redux login state

--- client/src/components/_login/login.tsx
+++ client/src/components/_login/login.tsx
@@ -2,18 +2,29 @@ import * as React from 'react'
 import { ProfileProps } from '../containers/propContatiner'
 import { LoginForm } from './loginForm'
 import { Profile } from './profile'
-import { Navbar } from '../subComponents/navbar';
+import { RootState } from '../containers/redux/initialStates'
+import { useDispatch, useSelector } from 'react-redux';
+import { login } from '../containers/redux/actionCreators'
 
 // conditional rendering based on login status
 export function Login() {
   const [user, setUser] = React.useState<ProfileProps | null>(null)
-  const tempLogin = false
+
+  // getting state from Redux store : useSelector
+  const isLogin = useSelector<RootState>((state) => state.login.isLogin)
+
+  // getting dispatch from Redux store : useDispatch
+  const dispatch = useDispatch()
+
+  console.log(isLogin)
 
   return (
-    <>
+    <>  
+      <button onClick={()=>dispatch(login())}>Login test with Redux</button>
+      
       {/* if user exists(logged in), render Profile component */}
       {/* if not(logged out / null), render LoginForm component */}
-      {tempLogin ? <Profile 
+      {isLogin ? <Profile 
                       name={"Jake"}
                       email={"testing@gmail.com"}
                       isLoggedIn={true}/> 
