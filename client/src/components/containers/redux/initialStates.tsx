b3b27c289e15a01b4af56624a31edcfaf8b3ef93
designerasun <asuntheawesome@gmail.com>
2022-01-28 14:38:54 +0900

    feat : initial states for cart items and other

--- client/src/components/containers/redux/initialStates.tsx
+++ client/src/components/containers/redux/initialStates.tsx
@@ -1,15 +1,6 @@
-// Use this RootState for useSelector hook
-export interface RootState  {
-    login : initialLoginStateProps
-    cart : initialCartStateProps<Products>
-}
+import { v4 } from "uuid"
 
-export interface Users { 
-    username : string
-    email : string
-    password : string
-}
-
+// Set login state
 export interface initialLoginStateProps { 
     isLogin : boolean
 }
@@ -18,21 +9,49 @@ export const initialLoginState:initialLoginStateProps = {
     isLogin : false,
 }
 
-export interface Products { 
-    name : string
-    author : string
-    date : string
-    price : number
+// Set user state
+export interface Users { 
+    username : string
+    email : string
+    password : string
 }
 
-export interface initialCartStateProps<Products> { 
-    loading : false
-    data : Products[]
-    errorMessage : ''
-}
-
-export const initialProductState:initialCartStateProps<Products> = { 
+export interface initialUserStateProps {
     loading : false, 
-    data : [], 
+    data : Users[], 
     errorMessage : ''
 }
+
+// Set cart item state
+export interface initialCartItemStateProps {
+    image : string
+    price : number
+    quantity : number
+    id : string | number
+}
+
+export const initialCartItemState : initialCartItemStateProps[] = [
+    { image : '' , price : 1, quantity : 1, id : v4() },
+    { image : '' , price : 3, quantity : 4, id : v4() },
+    { image : '' , price : 4, quantity : 10, id : v4() },
+    { image : '' , price : 13, quantity : 2, id : v4() },
+]
+
+// export interface Products { 
+//     name : string
+//     author : string
+//     date : string
+//     price : number
+// }
+
+// export interface initialCartStateProps<Products> { 
+//     loading : false
+//     data : Products[]
+//     errorMessage : ''
+// }
+
+// export const initialProductState:initialCartStateProps<Products> = { 
+//     loading : false, 
+//     data : [], 
+//     errorMessage : ''
+// }
