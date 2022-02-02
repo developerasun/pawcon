c9abe42524d0c851039e5ecbab22ab76616fe875
designerasun <asuntheawesome@gmail.com>
2022-02-02 16:15:15 +0900

    chore : JSONbin.io api url fix

--- client/src/components/apis/api.tsx
+++ client/src/components/apis/api.tsx
@@ -1,4 +1,5 @@
+// JSON files stored in JSONbin.io
 export const API_URL = { 
-    artworks : "https://api.jsonbin.io/b/61f9268ffb3ece3ad7cf6968", 
+    artworks : "https://api.jsonbin.io/b/61fa2b59518e5f3b2ab4a6a5", 
     users : "", 
 }
