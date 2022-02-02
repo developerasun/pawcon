4b4e5161992bf02a0d85ec73d8f430467164e999
designerasun <asuntheawesome@gmail.com>
2022-02-03 00:26:18 +0900

    feat : login cookie added with react-cookie

--- client/src/components/_login/login.tsx
+++ client/src/components/_login/login.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react'
-import { ProfileProps } from '../containers/propContatiner'
 import { LoginForm } from './loginForm'
 import { Profile } from './profile'
-import { RootState } from '../containers/redux/initialStates'
 import { useDispatch, useSelector } from 'react-redux';
 import { login } from '../containers/redux/actionCreators'
+import { RootState } from '../containers/redux/store.hooks';
+import { Cookies } from 'react-cookie';
+import { cookieList } from '../containers/cookieContainer'
 
 // conditional rendering based on login status
 export function Login() {
-  const [user, setUser] = React.useState<ProfileProps | null>(null)
+  const cookie = new Cookies()
+  console.log(cookie.get(cookieList.LOGIN)) // undefined if not logged in
+  const loginCookie = cookie.get(cookieList.LOGIN)
 
   // getting state from Redux store : useSelector
   const isLogin = useSelector<RootState>((state) => state.login.isLogin)
@@ -22,9 +25,10 @@ export function Login() {
     <>  
       <button onClick={()=>dispatch(login())}>Login test with Redux</button>
       
-      {/* if user exists(logged in), render Profile component */}
-      {/* if not(logged out / null), render LoginForm component */}
-      {isLogin ? <Profile 
+      {/* if user exists(jwt cookie exists), render Profile component */}
+      {/* if not, render LoginForm component */}
+      {loginCookie!==undefined 
+                ? <Profile 
                       name={"Jake"}
                       email={"testing@gmail.com"}
                       isLoggedIn={true}/> 
