817e025dac050816b7856258b64ad1c4f6d21361
designerasun <asuntheawesome@gmail.com>
2022-02-02 23:22:57 +0900

    feat : jwt logout added

--- client/src/components/_login/profile.tsx
+++ client/src/components/_login/profile.tsx
@@ -1,11 +1,23 @@
 import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { ProfileProps } from '../containers/propContatiner';
 
 export function Profile ({ name, email, isLoggedIn }: ProfileProps) {
+  const [submit, setSubmit] = React.useState(false);
+  const navigate = useNavigate()
+  React.useEffect(()=>{
+    if (submit) {
+      fetch('/logout')
+
+      alert('logout success')
+      navigate('/')
+    }
+  }, [submit, navigate])
   return (
       <ul>
         <li>Hello, {name}. Welcome back!</li>
         <li>ID : {email}</li>
+        <button type="submit" onClick={()=>setSubmit(true)}>Logout</button>
       </ul> 
   );
 }
