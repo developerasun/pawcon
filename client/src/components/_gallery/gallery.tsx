e6b116921dd765d5f2a911dced384ff01c755c5a
designerasun <asuntheawesome@gmail.com>
2022-02-03 23:04:34 +0900

    feat : artwork fetched from server api, pagination disabled added

--- client/src/components/_gallery/gallery.tsx
+++ client/src/components/_gallery/gallery.tsx
@@ -2,33 +2,25 @@ import * as React from 'react'
 import './sass/css/gallery.css'
 import { v4 as uuidV4 } from 'uuid'
 import { GalleryCards } from './galleryCards'
-import { ArtworkContext } from '../contexts/artworkContext'
 import { ImgBanner } from '../subComponents/banner'
 import { IMG_BANNER } from '../containers/imgContainer'
-import axios from 'axios'
-import { API_URL } from '../apis/api'
 import { ArtworkList } from '../apis/useFetch'
 
 // Set initial state for useState/Typescript
 const defaultProps :ArtworkList = []
 
 export function Gallery () {
-  const artworks =  React.useContext(ArtworkContext);
   const [page, setPage] = React.useState(1)
-  const [test, setTest] = React.useState(defaultProps);
+  const [data, setData] = React.useState(defaultProps);
 
-  // fetching data
-  React.useEffect(()=> {
-    const data = fetch(API_URL.artworks)
-    data.then((res)=>res.json()).then((result)=> {
-      console.log(result)
-      setTest(result)
-    }).catch((err)=>console.log(err))
-    // (async () => {
-    //   const response = await fetchArtwork(API_URL.artworks)
-    //   console.log(response.data)
-    //   setTest(response.data)
-    // })()
+  React.useEffect(()=> {    
+    // fetching data with dynamic route(decided in server side)
+    const data = fetch(`/apis/artworks/${page}`)
+    data.then((res)=>res.json())
+        .then((result)=> {
+          console.log(result)
+          setData(result)
+        }).catch((err)=>console.log(err))
   }, [ page ])
 
   return (
@@ -42,34 +34,31 @@ export function Gallery () {
         <h2>Weekly Tops</h2>
         <span>Meet coolest paw-oneers</span>
         <span>search bar here</span>
-        <div
-          className='galleryCardContainer' >
-          {artworks.map((item) => {
-            return <GalleryCards 
-                    key={uuidV4()}
-                    title={item.title}
-                    description={item.description}
-                    author={item.author}
-                    image={item.image}
-                    date={item.date}
-                    id={item.id} /> })}
-        </div>
 
-        {/* NOT DONE : fetched data test here */}
-        <div className="apiTest">
-          <p>Fetch data below</p>
-            {/* convert fetched data to Array */}
-            {Array.from(test).map((item)=>{
-              return item.details.identity.author
-            })}
+        <div className="cardContainer">
+          {/* convert fetched data to Array */}
+          { !data && <p>No gallery items for now..</p>}
+          { data && Array.from(data).map((item)=>{
+                return <GalleryCards 
+                        key={uuidV4()}
+                        title={item.title}
+                        description={item.details.identity.description}
+                        author={item.details.identity.author}
+                        image={item.details.image.url}
+                        date={item.details.identity.date}
+                        id={uuidV4()} /> })}
         </div>
         
         <div 
-          className="galleryCardPagination">
+          className="pagination">
           <button 
+            // fix the hardcoded page later with react-query
+            disabled={page<=1}
             onClick={()=>setPage(Math.max(page - 1, 1))}>&#8249;</button>
           <p>{page}</p>
-          <button 
+          <button
+            // fix the hardcoded page later with react-query
+            disabled={page>=3}
             onClick={()=>setPage(page + 1)}>&#8250;</button>
         </div>
 
