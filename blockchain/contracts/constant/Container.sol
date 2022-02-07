20f54ca69a3a626432c396ab1991f6900f39ade9
designerasun <asuntheawesome@gmail.com>
2022-02-07 16:50:14 +0900

    feat : add contract common constants

---
+++ blockchain/contracts/constant/Container.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+enum Rarity { 
+    QUARTER_LOAF,
+    HALF_LOAF,
+    FULLY_LOAF
+}
+
+/// @title A title that should describe the contract/interface
+/// @author The name of the author
+/// @notice Explain to an end user what this does
+/// @dev Explain to a developer any extra details
+contract Container { 
+
+    // grouping by the same type(lowering gas)
+    bytes32 public constant creator = "Jake Sung"; 
+
+    uint256 public cost = 0.03 ether;
+    uint256 public constant whitelistMintingLimit = 3; 
+    uint256 public constant maxSupply = 10000;
+    uint256 inDay =  24 hours;
+    uint256 inMonth = 4 weeks;
+    uint256 inYear = inMonth * 12;
+}
