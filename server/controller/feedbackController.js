c086a40fdd353a61352668481616de1ed563ba06
designerasun <asuntheawesome@gmail.com>
2022-02-11 01:52:24 +0900

    feat : saving editor js paragraphs

--- server/controller/feedbackController.js
+++ server/controller/feedbackController.js
@@ -1,8 +1,28 @@
+const path = require('path')
+const pawconFeedback = require('../model/post')
+
 // feedback post from client
-const feedback_post = (req, res) => { 
-    // NOT DONE
-    const { feedbacks } = req.body
-    console.log(feedbacks)
+const feedback_post = async (req, res) => { 
+    // use try~catch to avoid unhandled Promise error
+    try {
+        const { onePost } = req.body // extract client data
+        console.log( typeof onePost)
+        console.log( onePost )
+        const description = onePost
+        // model.create is a short for creating model instance(document)
+        // and save method. property name should be the same as desribed in mongoose schema.
+        const instance = await pawconFeedback.create( { description } )
+    
+        // send response to client 
+        res.status(201).json(instance)
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+// come back here after client feedback ui
+const feedback_delete = (req, res) => {
+
 }
 
 module.exports = { 
