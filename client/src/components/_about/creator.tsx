65e5139b14db8421d234f10d520e6a5e4a42c570
designerasun <asuntheawesome@gmail.com>
2022-02-11 22:31:31 +0900

    refactor : props/text contents fixed

--- client/src/components/_about/creator.tsx
+++ client/src/components/_about/creator.tsx
@@ -3,10 +3,9 @@ import { AboutCardsProps } from '../containers/propContatiner';
 import { IMG_ABOUT_ICON, IMG_PROFILE } from '../containers/imgContainer';
 import { v4 } from 'uuid';
 import { Button } from '../subComponents/button';
-import { ImgBanner } from '../subComponents/banner';
 
 export const AboutCards = (
-    { date, title, description, image }: AboutCardsProps ) => {
+    { date, title, description, image, hasButton, buttonText, buttonUrl }: AboutCardsProps ) => {
   return (
     <div className="career">
         <div className="title">
@@ -25,17 +24,17 @@ export const AboutCards = (
             // if string, renders in paragraph
             : <p>{description}</p> }
         </div>
+        { hasButton && <Button btnText={buttonText} url={buttonUrl}/> }
     </div>
 )}
 
 export function Creator () {
   return (
-    <div className='creator'>
+    <div id='creator'>
 
-        <div className="profile">
+        <div id="profile">
             <h1>About Jake Sung</h1>
             <img
-                style={{"borderRadius" : "50%", "maxWidth" : "50%", "height" : "auto" }}
                 loading='lazy' 
                 src={IMG_PROFILE.jake} 
                 alt="Jake Sung profile" />
@@ -45,49 +44,40 @@ export function Creator () {
         {/* Career 1 : Front End Developer */}
         <AboutCards 
             date='May 2021 ~ Current'
-            title='Front End Developer'
-            description={["HTML5", "CSS3/Sass", "JS/TS", "React"]}
-            image={IMG_ABOUT_ICON.laptop} />
-        <Button 
-            btnText='Follow Github'
-            url='https://github.com/developerasun' />
+            title='Software Engineer'
+            description={["Full stack", "3000++ contributions less than one year"]}
+            image={IMG_ABOUT_ICON.laptop} 
+            hasButton={true}
+            buttonText={'Follow Github'} 
+            buttonUrl={'https://github.com/developerasun'} />
 
         {/* Career 2 : Etsy Seller */}
         <AboutCards 
             date='Sep 2020 ~ Current'
             title='Etsy Seller'
-            description={["Global sales ranking top 11%", "350++ sales", "Created 500++ shop items"]}
-            image={IMG_ABOUT_ICON.etsy} />
-        <Button 
-            btnText='Explore shop'
-            url='https://www.etsy.com/shop/DesignerAsun' />
+            description={["Global top 11% sales", "Created 500++ shop items"]}
+            image={IMG_ABOUT_ICON.etsy} 
+            hasButton={false} />
 
         {/* Career 3 : Content Creator */}
         <AboutCards 
+            hasButton={false}
             date='Sep 2020 ~ Current'
             title='Content Creator'
             description={[ 
-                "TikTok : 400K video views under 28 days.", 
-                "Instagram  : 2K++ followers", 
-                "Pinterest : Group board with 230++ collaborators", 
-                "Created 500++ social media contents"
+                "400K views under 28 days.", 
+                "2K++ followers from scratch", 
+                "230++ collaborators", 
+                "500++ media contents"
             ]}
             image={IMG_ABOUT_ICON.socialMedia} />
-        <Button 
-            btnText='Instagram'
-            url='https://www.instagram.com/designerasun/' />
-        <Button 
-            btnText='Tiktok'
-            url='https://www.tiktok.com/@owltheletterer' />
-        <Button 
-            btnText='Pinterest'
-            url='https://www.pinterest.com/designerasun/' />
 
         {/* Career 4 : Graphic Designer */}
         <AboutCards 
+            hasButton={false}
             date='Nov 2019 ~ Aug 2020'
-            title='Freelance Logo Designer'
-            description={["Self-taught calligrapher", "Created 100++ typographic logo remakes", ]}
+            title='Graphic Designer'
+            description={["Self-taught calligrapher", "Freelance logo designer", ]}
             image={IMG_ABOUT_ICON.graphicDesigner} />
     </div>
   );
