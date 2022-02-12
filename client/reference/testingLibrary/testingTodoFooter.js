e811958760ae12e8ba0cb2c347a9065127e90998
Jake Sung <asuntheawesome@gmail.com>
2022-02-13 00:21:26 +0900

    Delete client/reference directory

--- client/reference/testingLibrary/testingTodoFooter.js
+++
@@ -1,7 +0,0 @@
-function TestingTodoFooter( {incompleTasks} ) {
-    return ( 
-    <p style={{"display":"none"}}>{incompleTasks} {incompleTasks===1 ? "task left" : "tasks left" }</p>    
-     );
-}
-
-export default TestingTodoFooter;
