2287d12944158986cc248fc28125c2f3be3b1eb5
designerasun <asuntheawesome@gmail.com>
2022-02-14 21:42:44 +0900

    feat : shop component logic set

--- client/src/components/_shop/shop.tsx
+++ client/src/components/_shop/shop.tsx
@@ -1,21 +1,21 @@
 import * as React from 'react';
-import { Navbar } from '../subComponents/navbar';
 import { CartForm } from './cartForm';
 import { CartList } from './cartList';
 
-export interface IShopProps {
-}
-
-export function Shop (props: IShopProps) {
+export function Shop () {
   return (
     <div>
       <h1>shop route</h1>
         shoppable items list here
       <br /><br /><br /><br />
-
+      {/* 
+        1. render shoppable NFTs with search 
+        2. add the NFTs to cart in cart form
+        3. render added NFTs in cart list, if non, render "there is no item now"
+        4. adjust amount/price with Redux
+        5. when payment clicks, web3 metamask pops up
+      */}
       <CartForm />
-      <br /><br /><br /><br />
-      <CartList />
     </div>
   );
 }
