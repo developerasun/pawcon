209e03eb28699e070a50264514baa387c9b3fdb7
designerasun <asuntheawesome@gmail.com>
2022-02-16 12:16:55 +0900

    feat : add passport/google strategy

--- server/middlewares/passport.js
+++ server/middlewares/passport.js
@@ -11,11 +11,24 @@ const config = require('../config/config')
 
 // get auth user model
 const pawconGoogleOauthUser = require('../model/googleOauthUser')
-const pawconGithubOauthUser = require(path.join(__dirname, '..', 'model', 'githubOauthUser.js'))
+const pawconGithubOauthUser = require('../model/githubOauthUser')
 
-// serialize
-// deserialize
+// serialize : store user data into session. The result of the 
+// serializeUser method is attached to the session 
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
 
+// deserialize : retrieve data from the session
+passport.deserializeUser( async (id, done) => {
+    try { 
+        const user = await pawconGoogleOauthUser.findById(id)
+        done(null, user)
+    } catch(err) { 
+        console.log(err.message)
+        throw err
+    }
+})
 
 // NOT TESTED 
 // 1. get access token from Google Authorization Server 
@@ -23,12 +36,22 @@ passport.use(new googleStrategy({
     clientID : config.AUTH.GOOGLE.CLIENT_ID, 
     clientSecret : config.AUTH.GOOGLE.CLIENT_SECRET, 
     callbackURL : config.AUTH.GOOGLE.AUTH_REDIRECT
-}, function(accessToken, refreshToken, profile, cb) {
+}, async function(accessToken, refreshToken, profile, cb) {
 
     // model.create returns a void when callback parameter is given
     // if not, returns a Promise
     // username : String, googleId : String, thumnail : String
-    pawconGoogleOauthUser.create({googleId : profile.id}, function(err, user) {
-        return cb(err, user)
-    })
+    
+    const user = await pawconGoogleOauthUser.findById(profile.id)
+    if (user) cb(null, user)
+    else {
+        console.log(profile)
+        const newUser = await pawconGoogleOauthUser.create({
+            username : profile.username, 
+            googleId : profile.id, 
+            thumnail : profile.photos
+        })
+        cb(null, newUser)
+    }
+
 }))
