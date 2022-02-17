327a34a476604aaf04bd9649bf9fe055fa5364ea
designerasun <asuntheawesome@gmail.com>
2022-02-17 11:45:25 +0900

    feat : add passport redirect/cookie setting logic

--- server/router/oAuthRouter.js
+++ server/router/oAuthRouter.js
@@ -2,23 +2,36 @@
 const passport = require('passport')
 const path = require('path')
 const oAuthRouter = require('express').Router()
-const config = require(path.join(__dirname, '..', 'config', 'config.js'))
+const config = require('../config/config')
 
 // ========================= Google Ouath ========================= //
 // passport : middleware
 // '/oauth/google', '/oauth/github'
-oAuthRouter.get('/google', passport.authenticate('google', {
-    scope:['profile']
-}))
+oAuthRouter.get(
+    '/google', 
+    passport.authenticate('google', { scope:['profile'] }),
+)
 
 // should be the same with Authroized redirect URIs 
 // registered in Google Cloud Platform. '/oauth/google/redirect',
-console.log(config.AUTH_REDIRECT) 
+console.log(config.AUTH.GOOGLE.AUTH_REDIRECT) 
 
-oAuthRouter.get('/google/redirect', passport.authenticate('google'), (req, res) => {
-    // console.log(req.user.username)
+oAuthRouter.get(
+    '/google/redirect', 
+    passport.authenticate('google'), 
+    (req, res) => {
+    // set cookie for client
+    res.cookie(
+        config.AUTH.GOOGLE.COOKIE.NAME, 
+        config.AUTH.GOOGLE.COOKIE.VALUE, 
+        {
+            maxAge : config.AUTH.GOOGLE.COOKIE.EXPIRATION,
+            httpOnly : false, // make accessible to front end
+        }
+    )
     res.redirect('/') // should be configured with client(react router, react cookie) as well
-})
+    }
+)
 
 oAuthRouter.get('/logout', (req, res) => {
     req.logOut() // logout method is provided by passport
