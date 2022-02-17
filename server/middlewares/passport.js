f7a481d74902b88c6d0192a3e77ee30abbd2a399
designerasun <asuntheawesome@gmail.com>
2022-02-17 11:44:19 +0900

    feat : add oauth login logic

--- server/middlewares/passport.js
+++ server/middlewares/passport.js
@@ -30,28 +30,29 @@ passport.deserializeUser( async (id, done) => {
     }
 })
 
-// NOT TESTED 
 // 1. get access token from Google Authorization Server 
 passport.use(new googleStrategy({
     clientID : config.AUTH.GOOGLE.CLIENT_ID, 
     clientSecret : config.AUTH.GOOGLE.CLIENT_SECRET, 
     callbackURL : config.AUTH.GOOGLE.AUTH_REDIRECT
 }, async function(accessToken, refreshToken, profile, cb) {
-
-    // model.create returns a void when callback parameter is given
-    // if not, returns a Promise
-    // username : String, googleId : String, thumnail : String
-    
-    const user = await pawconGoogleOauthUser.findById(profile.id)
-    if (user) cb(null, user)
-    else {
-        console.log(profile)
-        const newUser = await pawconGoogleOauthUser.create({
-            username : profile.username, 
-            googleId : profile.id, 
-            thumnail : profile.photos
-        })
-        cb(null, newUser)
+    try { 
+        console.log(profile) // inspect profile object from Google Server
+        const user = await pawconGoogleOauthUser.findOne({googleId : profile.id}) // query by id
+        if (user) cb(null, user)
+        else {
+            // model.create returns a void when callback parameter is given
+            // if not, returns a Promise
+            // username : String, googleId : String, thumnail : String
+            const newUser = await pawconGoogleOauthUser.create({
+                username : profile.displayName, 
+                googleId : profile.id, 
+                thumnail : profile.photos[0].value
+            })
+            cb(null, newUser)
+        }
+    } catch (err) { 
+        console.log(err) 
+        throw err
     }
-
 }))
