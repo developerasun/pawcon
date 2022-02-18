8c2c5304b1e9d0ba1135d5c33a3c1b3291a6116f
designerasun <asuntheawesome@gmail.com>
2022-02-18 12:06:57 +0900

    feat : add metamask/paypal logic

---
+++ client/src/components/_shop/payment/metamask.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface IRenderMetaMaskProps {
+}
+
+export function RenderMetaMask (props: IRenderMetaMaskProps) {
+  return (
+    <div>
+      metamask logic here
+    </div>
+  );
+}
---
+++ client/src/components/_shop/payment/paypal.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface IRenderPaypalProps {
+}
+
+export function RenderPaypal (props: IRenderPaypalProps) {
+  return (
+    <div>
+      paypal logic here
+    </div>
+  );
+}
