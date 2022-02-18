5eafc1876a459b3feaf4ab7b2add276e7beee5e1
designerasun <asuntheawesome@gmail.com>
2022-02-18 12:06:26 +0900

    feat : add payment component with select/option

--- client/src/components/_shop/payment/payment.tsx
+++ client/src/components/_shop/payment/payment.tsx
@@ -1,15 +1,34 @@
 import * as React from 'react';
+import { RenderMetaMask } from './metamask'
+import { RenderPaypal } from './paypal'
+
 export interface IPaymentProps {
 }
 
 export function Payment (props: IPaymentProps) {
+    const [method, setMethod] = React.useState("")
+
+    const handleChange = () => {
+        const checkout = document.getElementById('checkout') as HTMLSelectElement
+        const selected = checkout.options[checkout.selectedIndex].value
+        setMethod(selected)
+    }
+
     return (
-    <div className="payment">
-        <p className="total"> CART TOTAL : {129}</p>
-        <label htmlFor="terms">
-            <input type="checkbox" name="terms" id="terms" /> I agree to Terms & Conditions
-        </label>
-        <button type="submit" onClick={()=>console.log()}>CHECKOUT</button>
-    </div> 
+    <>
+    <form id="payment">
+        <label htmlFor="termsAndCondition">Terms and condition</label>
+        <input type="checkbox" name="terms" id="termsAndCondition" /> I agree to Terms & Conditions
+        {/* add metamask and paypal logic here */}
+        <select onChange={handleChange} name="checkout" id="checkout">
+            <option value="Metamask">Metamask</option>
+            <option value="Paypal">Paypal</option>
+        </select>
+    </form> 
+    {method==="Metamask" 
+        ? <RenderMetaMask />
+        : <RenderPaypal />
+    }
+    </>
     );
 }
