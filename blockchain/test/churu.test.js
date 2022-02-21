7137c040d49d9013ad1c8ef8dc85471005f14cff
designerasun <asuntheawesome@gmail.com>
2022-02-21 15:28:59 +0900

    feat : adding test codes for tokens

---
+++ blockchain/test/churu.test.js
@@ -0,0 +1,8 @@
+const Churu = artifacts.require('Churu')
+
+contract('Churu', accounts => {
+    it('should mint a ERC20', () => {
+        // some logic here
+        // check this truffle example : https://trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript.html
+    })
+})
