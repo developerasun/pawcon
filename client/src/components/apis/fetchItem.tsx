e3cb32a0754b29d9c90edc09e9ea1045ba3adfd6
designerasun <asuntheawesome@gmail.com>
2022-02-22 20:11:47 +0900

    fix : delete unnecessary funcs

--- client/src/components/apis/fetchItem.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export async function fetchItem ( url : string) {
-  const response = await fetch(url)
-  const data = await response.json() 
-  return data // return Promise
-}
