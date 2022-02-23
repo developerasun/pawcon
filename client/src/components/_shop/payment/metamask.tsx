fd39c132aa78ed8142f2881e45fc86dd437db588
designerasun <asuntheawesome@gmail.com>
2022-02-23 14:50:56 +0900

    feat : add comments for contract method logic

--- client/src/components/_shop/payment/metamask.tsx
+++ client/src/components/_shop/payment/metamask.tsx
@@ -4,9 +4,14 @@ export interface IRenderMetaMaskProps {
 }
 
 export function RenderMetaMask (props: IRenderMetaMaskProps) {
+  const handleClick = () => {
+    // detect metamaks in user browser and get it as a provider
+    
+  }
   return (
     <div>
       metamask logic here
+      <button onClick={handleClick}>CHECKOUT</button>
     </div>
   );
 }
