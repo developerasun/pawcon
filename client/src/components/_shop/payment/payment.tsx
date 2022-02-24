91fb1dab122429b8af535a71ac0ed710b858ff5e
designerasun <asuntheawesome@gmail.com>
2022-02-24 21:59:16 +0900

    feat : add payment selection

--- client/src/components/_shop/payment/payment.tsx
+++ client/src/components/_shop/payment/payment.tsx
@@ -1,34 +1,38 @@
 import * as React from 'react';
 import { RenderMetaMask } from './metamask'
 import { RenderPaypal } from './paypal'
-
-export interface IPaymentProps {
-}
-
-export function Payment (props: IPaymentProps) {
-    const [method, setMethod] = React.useState("")
-
-    const handleChange = () => {
-        const checkout = document.getElementById('checkout') as HTMLSelectElement
-        const selected = checkout.options[checkout.selectedIndex].value
-        setMethod(selected)
-    }
+import '../sass/css/payment.css'
+export function Payment () {
+    const [method, setMethod] = React.useState("Metamask") // payment default is Metamask
+    const handleClick = ({currentTarget : { id }}: React.MouseEvent) => { setMethod(id) }
 
     return (
     <>
-    <form id="payment">
-        <label htmlFor="termsAndCondition">Terms and condition</label>
-        <input type="checkbox" name="terms" id="termsAndCondition" /> I agree to Terms & Conditions
-        {/* add metamask and paypal logic here */}
-        <select onChange={handleChange} name="checkout" id="checkout">
-            <option value="Metamask">Metamask</option>
-            <option value="Paypal">Paypal</option>
-        </select>
-    </form> 
-    {method==="Metamask" 
-        ? <RenderMetaMask />
-        : <RenderPaypal />
-    }
+    <div
+        id='payment' 
+        className="payment">
+        <div id="texts">
+            <span>Payment Method</span>
+            <p>Please select a preferable payment method</p>
+        </div>
+        <div id="methods">
+            <label htmlFor="Metamask" className='labels'>
+                <img src="https://i.ibb.co/Qchvsh4/metamask-logo.webp" alt="metamask logo" id='metamaskLogo' />
+                <input type="radio" name="payment" id="Metamask" onClick={(e)=>handleClick(e)} />
+            </label>
+            <label htmlFor="Paypal" className='labels'>
+                <img src="https://i.ibb.co/GQFC9dH/paypal-logo.webp" alt="paypal logo" id='paypalLogo' />
+                <input type="radio" name="payment" id="Paypal" onClick={(e)=>handleClick(e)} />
+            </label>
+        </div>
+        <div id="buttons">
+            {method==="Metamask" 
+                ? <RenderMetaMask />
+                : <RenderPaypal />
+            }
+        </div>
+    </div>
+
     </>
     );
 }
