9cdc5d62078842c10619b1bec73738e90cd6b6c5
designerasun <asuntheawesome@gmail.com>
2022-02-26 01:05:43 +0900

    fix : set httpOnly to true, res.locals

--- server/router/oAuthRouter.js
+++ server/router/oAuthRouter.js
@@ -14,22 +14,24 @@ oAuthRouter.get(
 
 // should be the same with Authroized redirect URIs 
 // registered in Google Cloud Platform. '/oauth/google/redirect',
-console.log(config.AUTH.GOOGLE.AUTH_REDIRECT) 
 
 oAuthRouter.get(
     '/google/redirect', 
     passport.authenticate('google'), 
     (req, res) => {
-    // set cookie for client
-    res.cookie(
+        // set cookie for client
+        res.cookie(
         config.AUTH.GOOGLE.COOKIE.NAME, 
         config.AUTH.GOOGLE.COOKIE.VALUE, 
         {
             maxAge : config.AUTH.GOOGLE.COOKIE.EXPIRATION,
-            httpOnly : false, // make accessible to front end
-        }
-    )
-    res.redirect('/') // should be configured with client(react router, react cookie) as well
+            httpOnly : true, // prevent client-JS approach to cookie
+        })
+    console.log(config.AUTH.GOOGLE.AUTH_REDIRECT) 
+    
+    // TEST THIS : check if this works in client res.json()
+    res.locals.testVariable = 'test'
+    res.status(200).redirect('/') // should be configured with client(react router)
     }
 )
 
