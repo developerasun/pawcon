0a355be9464b4551a3d49e1975fd61435a6816ed
designerasun <asuntheawesome@gmail.com>
2022-02-28 21:30:51 +0900

    fea t: add onClick property to custom button comp

--- client/src/components/subComponents/button.tsx
+++ client/src/components/subComponents/button.tsx
@@ -2,9 +2,12 @@ import * as React from 'react';
 import './sass/css/button.css'
 import { IButtonProps } from '../containers/C_props'
 
-export function Button ({ btnText, url }: IButtonProps) {
+export function Button ({ btnText, url, callback }: IButtonProps) {
     return (
-        <button className='buttonComponent' type='submit'>
+        <button 
+            onClick={callback}
+            className='buttonComponent' 
+            type='submit'>
             <a href={url} target={"_blank"} rel="noreferrer">
             {btnText}
             </a>
