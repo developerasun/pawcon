398d14da26ad04ff8079f067ba9a3202d573be24
designerasun <asuntheawesome@gmail.com>
2022-02-28 21:27:37 +0900

    feat : render socket io client, add react-avatar

--- client/src/components/_community/chat.tsx
+++ client/src/components/_community/chat.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import  { io }  from 'socket.io-client';
 import { v4 } from 'uuid';
+import './sass/css/chat.css';
+import Avatar, { genConfig } from 'react-nice-avatar'; 
 
 const socket = io('http://localhost:3001')
 socket.on('connect', ()=>console.log(socket.id))
@@ -9,75 +11,75 @@ socket.on('disconnect',()=>console.log('user disconnected'))
 interface ChatMessageProps {
   userName : string
   message : string
+  senderId : string
 }
 
 export function Chat () {
-  const [submit, setSubmit] = React.useState(false)
   const [chat, setChat] = React.useState<ChatMessageProps[]>([])
+  // create a random avatar with react-nice-avatar
+  const config = genConfig() 
 
-  // fix infinite render bug
-  React.useEffect(() => {
-    if (submit) {
-      const messages = document.getElementById('messages') as HTMLUListElement
-      socket.on('chat', (data:ChatMessageProps) => {
-        if (messages) { // type guard
-          setChat(prev => {
-            return [...prev, { 
-              userName : data.userName,
-              message : data.message 
-            }]
-          })
-          // const li = messages.appendChild(document.createElement('li'))
-          // li.innerText = `${data.userName} : ${data.message}`
-          // messages.innerHTML += `<li><p>${data.userName} : ${data.message}</p></li>`
-        }
-      })
-    }
-
-    window.addEventListener('unload', () => { 
-      // disconnect socket when user leaves page
-      socket.disconnect()
-    })
-  }, [submit])
-
-  React.useEffect(() => {
-    const message = document.getElementById('message') as HTMLInputElement
-    const userName = document.getElementById('userName') as HTMLInputElement
-    if (message && userName) { // type guard
-        // socket.emit(event name, event value) => event value can be anything. no limit.
-        socket.emit('chat', { // this event name should be the same in server side
-          userName : userName.value,
-          message : message.value 
-        })
-      }
-    message.value = "" // reinitialize input value
-  })
+  // useRef hook preserves an initial value. 
+  // should preserve this since chat renders each time. 
+  const avatar = React.useRef(
+    <li>
+      <Avatar  
+      sex='man'
+      style={{width : '3rem', height : '3rem'}} />
+    </li>)
 
   const handleSubmit = (event : React.FormEvent) => {
     event.preventDefault()
-    setSubmit(!submit)
-  }    
+    const message = document.getElementById('message') as HTMLInputElement
+    const userName = document.getElementById('userName') as HTMLInputElement
+
+    if (message && userName) { // type guard
+      // socket.emit(event name, event value) => event value can be anything. no limit.
+      socket.emit('chat:send', { // this event name should be the same in server side
+        userName : userName.value,
+        message : message.value, 
+        senderId : 'clientSender'
+      })
+    }
+    message.value = "" // reinitialize input value
+  }
+
+  React.useEffect(() => {
+    socket.on('chat:receive', ({userName, message, senderId}:ChatMessageProps) => {
+      // prevent duplicated render
+      if (senderId === 'serverSender') {
+        console.log(message)
+        setChat((prev) => ([
+          ...prev, 
+          {userName, message, senderId}
+        ]))
+      }
+    })
+  }, [])
 
   return (
-    <div>
-      <ul id="messages">{chat.map((item) => {
-        return <li key={v4()}>{item.userName} : {item.message}</li>
-      })}</ul>
+    <div id='chatContainer'>
 
-      {/* {chat} */}
-      
-      <form id="form" onSubmit={(event) => handleSubmit(event)}>
+      <ul id="messages">
+        <li id='avatar'>{avatar.current}</li>
+        {chat.map((item) => {
+          return ( <li key={v4()} className='chatMessage'> {item.userName} : {item.message} </li> ) })}
+      </ul>
+
+      <form id="form" onSubmit={handleSubmit}>
         {/* add functionality that disalbes user name input once decided */}
-        <input 
+        <input
           required
+          name='userName'
           id="userName" 
           type={'text'} 
           placeholder='Set user name' /> 
         <input
           required 
+          name='message'
           id="message" 
           type={'text'} 
-          placeholder={'Type message'}
+          placeholder={'Write something ...'}
           autoComplete='off' />
         <button>Send</button>
       </form>
