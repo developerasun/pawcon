06421862fd75b2a0966f7c645e314eb41e5f6a25
designerasun <asuntheawesome@gmail.com>
2022-03-04 01:13:00 +0900

    feat : sign up validation(email check, password length) added

--- client/src/components/_signup/signup.tsx
+++ client/src/components/_signup/signup.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import pawconLogo from '../../assets/img/logo/pawcon-logo.webp'
+import { API_DEV } from '../containers/C_apiUrl';
 import './sass/css/signupForm.css'
+import { LoginValidationError } from '../_login/loginForm'
 
 export function SignUp () {
   const navigate = useNavigate()
   const [submit, setSubmit] = React.useState(false)
+  const [validationErr, setValidationErr] = React.useState<LoginValidationError>()
+
   const handleSubmit = (event : React.FormEvent) => { 
     event.preventDefault()
     setSubmit(true)
@@ -20,7 +23,7 @@ export function SignUp () {
 
     // replace this part with Axios
     if (submit) { 
-      fetch('/signup', {
+      fetch(API_DEV.signup, {
         method: 'POST', 
         body: JSON.stringify({
           email : email.value, 
@@ -28,26 +31,35 @@ export function SignUp () {
         }), 
         headers : { 'Content-Type' : 'application/json' }, 
         signal : abortController.signal
-      }).then((res) => {
-        if (res.ok) { return res.json()}
+      }).then(async (res) => {
+        // check if the id is created, status code should be synced with server
+        if (res.status === 201) { return res.json() } 
+        if (res.status === 400) {
+          const error = await res.json()
+          setValidationErr(error)
+        }
       }).then((data)=>{
-        console.log(data)
-        alert("sign up success")  
-        navigate('/')
+        console.log(data) // data object includes success property(set by server)
+        if (data.success) { 
+          alert("sign up success")  
+          navigate('/')
+        } 
+      }).catch((err) => {
+        console.log(err)
       })
     }
 
     // cleanup
     return () => abortController.abort()
-  }, [submit]) // add dependency here
+  }, [submit, navigate]) // add dependency here
 
   return (
 <div className='signupComponent'>
   
   <div className="signupPromotionContainer">
     <h2>Join PawCon today!</h2>
-    <p>Be the one of the coolest paw-oneers in seconds.</p>
-    <img src={pawconLogo} width='200' alt='pawcon logo'/>
+    <p>Be NFT Paw-oneers in seconds.</p>
+    <img src={"https://i.ibb.co/bvCbscp/paw-login.webp"} width='200' alt='pawcon logo'/>
   </div>
 
   <div className="signupContainer">
@@ -56,19 +68,33 @@ export function SignUp () {
         onSubmit={handleSubmit}>
       <label htmlFor="email">
         <input 
-            type='email' 
-            name='email'
-            id='email'      
-            placeholder='Eamil address'
-            required />
+          className={
+            validationErr ?
+            validationErr.errorMessage.includes("email") ?
+            'emailError' : ''
+            : '' }
+          type='email' 
+          name='email'
+          id='email'      
+          placeholder='Eamil address'
+          required />
       </label>
       <label htmlFor="password">
-        <input type='password'  
-            name='password'
-            id='password'      
-            placeholder='Password'
-            required />
+        <input 
+          className={
+            validationErr ?
+            validationErr.errorMessage.includes("Password") ? // case sensitive, can't use reg exp
+            'passwordError' : ''
+            : '' }
+          type='password'  
+          name='password'
+          id='password'      
+          placeholder='Password'
+          required />
       </label> 
+      <span style={validationErr && {"color": "darkred" , "fontWeight" : "bold"}}>
+        {validationErr ? validationErr.errorMessage : "Please sign up first"}
+      </span>
       <button className='signupBtn' type='submit'>Sign up</button>
     </form>
     <Link to={`/login`} className='loginLink'><i><u>Already got account?</u></i></Link>
