064e0a4e53f2a25513426a7ace8210b03b6bd5f0
designerasun <asuntheawesome@gmail.com>
2022-03-04 01:18:40 +0900

    feat : req.app.locals added for passport user info

--- server/router/oAuthRouter.js
+++ server/router/oAuthRouter.js
@@ -5,16 +5,15 @@ const oAuthRouter = require('express').Router()
 const config = require('../config/config')
 
 // ========================= Google Ouath ========================= //
-// passport : middleware
 // '/oauth/google', '/oauth/github'
 oAuthRouter.get(
     '/google', 
     passport.authenticate('google', { scope:['profile'] }),
 )
 
+// route : /oauth/google/redirect
 // should be the same with Authroized redirect URIs 
 // registered in Google Cloud Platform. '/oauth/google/redirect',
-
 oAuthRouter.get(
     '/google/redirect', 
     passport.authenticate('google'), 
@@ -27,13 +26,28 @@ oAuthRouter.get(
             maxAge : config.AUTH.GOOGLE.COOKIE.EXPIRATION,
             httpOnly : true, // prevent client-JS approach to cookie
         })
-    console.log(config.AUTH.GOOGLE.AUTH_REDIRECT) 
     
-    // TEST THIS : check if this works in client res.json()
-    res.locals.testVariable = 'test'
+        if (req.user !== null) { 
+            console.log("user created by passport : ",req.user) // passport save user info into request.user(Express.user)
+            // set passport user info to app local variable
+            req.app.locals.user = req.user
+        }
+
     res.status(200).redirect('/') // should be configured with client(react router)
-    }
+    }, 
 )
+
+// route : /oauth/google/user
+// FIX : res.app.locals stays the same,  
+oAuthRouter.get('/google/user', (req, res) => {
+    // check if passport user object exists in req.app.locals
+    if (req.app.locals.user) { 
+        res.json({success : true, user : req.app.locals.user})
+        req.app.locals.user = null
+    } else { 
+        res.status(401).json({ success : false, user : null })
+    }
+})
 
 oAuthRouter.get('/logout', (req, res) => {
     req.logOut() // logout method is provided by passport
