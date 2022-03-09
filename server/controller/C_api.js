61bf7f496b456c053b6a278d6d8f4760e21be4ce
designerasun <asuntheawesome@gmail.com>
2022-03-09 15:53:24 +0900

    refactor : move getting google user to api controller, fix : add google user to config

--- server/controller/C_api.js
+++ server/controller/C_api.js
@@ -1,10 +1,23 @@
+const config = require('../config/config')
+const pawconGoogleUser = require('../model/googleOauthUser')
+
 const path = require('path')
 const fs = require('fs')
 
-const getUsers = (req, res) => {
-    res.set('Content-Type', 'application/json')
-    // convert buffer to string
-    res.status(200).send(users.toString())
+// send user to client if exists
+const getGoogleOauthUser = async (req, res) => {
+    try {
+        // not logged out and user info saved in googleUser variable
+        if (config.AUTH.GOOGLE.USER !== null) {
+            const user = await pawconGoogleUser.findOne({googleId : config.AUTH.GOOGLE.USER.googleId})
+            res.status(200).json({ success : true, username : user.username, thumnail : user.thumnail })
+        } else { 
+            console.log("no google user in req object")
+            res.status(401).json( { success : false, user : null } )
+        }
+    } catch(err) {
+        console.log(err)
+    }
 }
 
 // dynamic routes for artworks
@@ -15,6 +28,6 @@ const getArtworks = (req, res) => {
 }
 
 module.exports = { 
-    getUsers, 
+    getGoogleOauthUser, 
     getArtworks
 }
--- server/controller/C_oAuth.js
+++ server/controller/C_oAuth.js
@@ -1,7 +1,4 @@
 const config = require('../config/config')
-const pawconGoogleUser = require('../model/googleOauthUser')
-
-let googleUser = null // set initial google login user
 
 const googleOauthRedirect_get = (req, res, next) => {
     // set login cookie
@@ -16,36 +13,19 @@ const googleOauthRedirect_get = (req, res, next) => {
     if (req.user !== null) { 
         console.log("user created by passport : ",req.user) // passport save user info into request.user(Express.user)
         console.log("checking passport session : ", req.session.passport.user)
-        googleUser = req.user // set google user from passport
+        config.AUTH.GOOGLE.USER = req.user // set google user from passport
     }
     res.status(200).redirect('/') // should be configured with client(react router)
 }
 
-// send user to client if exists
-const googleOauthUser_get = async (req, res) => {
-    try {
-        // not logged out and user info saved in googleUser variable
-        if (googleUser !== null) {
-            const user = await pawconGoogleUser.findOne({googleId : googleUser.googleId})
-            res.json({ success : true, username : user.username, thumnail : user.thumnail })
-        } else { 
-            console.log("no google user in req object")
-            res.status(401).json( { success : false, user : null } )
-        }
-    } catch(err) {
-        console.log(err)
-    }
-}
-
 const googleOauthUserLogout_get = (req, res) => {
     req.logOut() // delete req.user property and clear login session
-    googleUser = null // reset google user info
+    config.AUTH.GOOGLE.USER = null // reset google user info
     res.redirect('/')
 }
 
 
 module.exports = {
-    googleOauthUser_get, 
     googleOauthRedirect_get, 
     googleOauthUserLogout_get
 }
