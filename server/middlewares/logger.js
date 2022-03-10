57e3293fea15d1810a89493fbdff02da3bd7084c
designerasun <asuntheawesome@gmail.com>
2022-03-10 12:10:09 +0900

    feat : add custom morgan for logging

--- server/middlewares/logger.js
+++ server/middlewares/logger.js
@@ -1,14 +1,15 @@
+const morgan = require('morgan')
+const path = require('path')
+const baseName = path.basename(__dirname)
 // const morgan = require('morgan') // logger dependency
 
-// logger takes a custom parameter and 
-// wraps req,res middleware 
-const logger = (where) => {
-    // use the custom parameter
-    console.log(`logger coming from : ${where}`)
+// FIX: fix middleware hanging error 
+// logger takes a custom parameter and wraps req,res middleware 
+const logger = (option) => {
     return (req, res, next) => {
-
-        // move to next middleware
-        next()
+        console.log(`logger dir : ${baseName}`)
+        morgan(option) // tiny, short, dev, common, combined
+        next() // move to next middleware
     }   
 }
 
