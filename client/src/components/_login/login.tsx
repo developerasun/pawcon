8a44860b3acf38e235a066ef302a52773819f97c
designerasun <asuntheawesome@gmail.com>
2022-03-10 12:02:02 +0900

    feat : jwt/google login condition added

--- client/src/components/_login/login.tsx
+++ client/src/components/_login/login.tsx
@@ -5,19 +5,13 @@ import { RootState, useAppSelector } from '../containers/redux/store.hooks';
 
 export function Login() {
   const isLogin = useAppSelector((state:RootState) => state.login.isLogin)
-
-  // FIX : check oauth status
-  React.useEffect(() => {
-    console.log('value of isLogin is : ', isLogin)
-  }, [isLogin])
+  const isGoogleLogin = useAppSelector((state: RootState) => state.googleLogin.success)
 
   return (
     <>  
       {/* if user exists(login with jwt or oauth), render Profile component */}
       {/* if not, render LoginForm component */}
-      {isLogin
-                ? <Profile/> 
-                : <LoginForm />}
+      {isLogin !== true && isGoogleLogin !== true ? <LoginForm/> : <Profile />}
     </>
   );
 }
