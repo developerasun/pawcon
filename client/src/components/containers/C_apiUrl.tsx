2107eea0b3832432b116f0fbfbd14456bbe5047a
designerasun <asuntheawesome@gmail.com>
2022-03-10 12:07:48 +0900

    fix : jwt/google logout, user api url fixed

--- client/src/components/containers/C_apiUrl.tsx
+++ client/src/components/containers/C_apiUrl.tsx
@@ -19,10 +19,12 @@ export const API_DEV = {
         baseUrl : `http://localhost:3001/apis/users/` 
     }, 
     login : "http://localhost:3001/login",
+    logout : "http://localhost:3001/logout",
     oauth : { 
         google : { 
             url : "http://localhost:3001/oauth/google", 
-            user : "http://localhost:3001/oauth/google/user"
+            user : "http://localhost:3001/apis/users/google", 
+            logout : "http://localhost:3001/oauth/google/logout"
         }, 
         github : ""
     },
