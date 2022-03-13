f921b372b31685dc4f1dfef719cf12c53b578750
designerasun <asuntheawesome@gmail.com>
2022-03-14 01:44:56 +0900

    fix : delete playground.tsx, fix router comp path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,33 +11,30 @@
                                                       | $$                                                                 
                                                       |__/                                                                 
 */
-
-import React from 'react';
 import './App.css'
 // React router v6
 // Outlet : Renders the child route's element, if there is one.
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './components/containers/redux/store'
-import { Home } from './components/_home/home'
-import { About } from './components/_about/about'
-import { Community } from './components/_community/community'
-import { Create } from './components/_create/create'
-import { Gallery } from './components/_gallery/gallery'
-import { Details } from './components/_gallery/details'
-import { RenderDetails } from './components/_gallery/renderDetails'
-import { Shop } from './components/_shop/shop'
-import { Login } from './components/_login/login'
-import { SignUp } from './components/_signup/signup'
+import { Home } from './components/pages/_home/home'
+import { About } from './components/pages/_about/about'
+import { Community } from './components/pages/_community/community'
+import { Create } from './components/pages/_create/create'
+import { Gallery } from './components/pages/_gallery/gallery'
+import { Details } from './components/pages/_gallery/details'
+import { RenderDetails } from './components/pages/_gallery/renderDetails'
+import { Shop } from './components/pages/_shop/shop'
+import { Login } from './components/pages/_login/login'
+import { SignUp } from './components/pages/_signup/signup'
 import { Page404 } from './components/subComponents/page404'
 import { Footer } from './components/subComponents/footer'
 import { Navbar } from './components/subComponents/navbar'
 
 function App() {
-  return (
+  return ( 
     // react-redux store
     <Provider store={store}>
-      {/* context api */}
       {/* react-router */}
       <BrowserRouter>
         <Navbar />
@@ -59,9 +56,8 @@ function App() {
             <Route path='/signup' element={<SignUp />}/>
             <Route path='*' element={<Page404 />}/>
           </Routes>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
-
     </Provider>
   );
 }
