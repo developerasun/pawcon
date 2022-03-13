fa363ce99375aa9b6d9e9974ceb49db1cbb38c8b
designerasun <asuntheawesome@gmail.com>
2022-03-14 01:38:50 +0900

    fix : minor changes

--- client/src/components/containers/C_cookie.tsx
+++
@@ -1,4 +0,0 @@
-export const cookieList = {
-    LOGIN : 'pawconJsonwebtoken', // login jwt cookie
-    OAUTH : 'pawconOauthtoken' // login oauth cookie
-}
--- client/src/components/containers/redux/actionTypes.tsx
+++ client/src/components/containers/redux/actionTypes.tsx
@@ -11,3 +11,5 @@ export const INCREASE_QTY = 'INCREASE_QTY'
 export const DECREASE_QTY = 'DECREASE_QTY'
 export const ADD_TO_CART = 'ADD_TO_CART'
 export const REMOVE_FROM_CART = 'REMOVE_FROM_CART'
+
+// TO DO : add artwork action types
