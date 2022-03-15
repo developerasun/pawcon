0afc3bacaf91c70ff06fe911b879b5358a106846
designerasun <asuntheawesome@gmail.com>
2022-03-15 14:03:33 +0900

    feat: add logger middlewares, fix: minor changes

--- server/router/R_login.js
+++ server/router/R_login.js
@@ -1,13 +1,18 @@
 // login/logout router here
 const path = require('path')
 const loginPath = path.join('..', 'controller', 'C_login.js')
+const fileName = path.basename(__filename)
+const logger = require('../middlewares/logger')
+const morgan = require('morgan')
 
 const loginRouter = require('express').Router()
 const loginController = require(loginPath)
 
+loginRouter.use(morgan('tiny'))
+loginRouter.use(logger(fileName))
+
 loginRouter.post('/login', loginController.login_post)
 loginRouter.get('/logout', loginController.logout_get)
-
 loginRouter.post('/signup', loginController.signup_post)
 
 module.exports = loginRouter
--- server/router/R_oAuth.js
+++ server/router/R_oAuth.js
@@ -1,22 +1,26 @@
 const passport = require('passport')
+const path = require('path')
+const fileName = path.basename(__filename)
 const oAuthRouter = require('express').Router()
 const oAuthController = require('../controller/C_oAuth')
+const morgan = require('morgan')
+const logger = require('../middlewares/logger')
+
+oAuthRouter.use(morgan('tiny'))
+oAuthRouter.use(logger(fileName))
 
 // ========================= Google Ouath ========================= //
 // base route : /oauth
 oAuthRouter.get(
     '/google', 
-    passport.authenticate('google', { scope:['profile'] }),
-)
+    passport.authenticate('google', { scope:['profile'] }) )
 
 // redirect route should be the same with Authroized redirect URIs 
 // registered in Google Cloud Platform. '/oauth/google/redirect',
 oAuthRouter.get(
     '/google/redirect', 
-    passport.authenticate('google'), oAuthController.googleOauthRedirect_get 
-)
+    passport.authenticate('google'), oAuthController.googleOauthRedirect_get)
 
-// FIX : fix internal server 500 error
 oAuthRouter.get('/google/logout', oAuthController.googleOauthUserLogout_get)
 // ========================= Google Ouath ========================= //
 
