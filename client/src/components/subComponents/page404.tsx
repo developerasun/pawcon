72b955d904de745a8d5a0a1bf0aa53d18b7481f5
designerasun <asuntheawesome@gmail.com>
2022-03-17 01:42:59 +0900

    feat: page404 img banner added

--- client/src/components/subComponents/page404.tsx
+++ client/src/components/subComponents/page404.tsx
@@ -1,22 +1,16 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
+import { ImgBanner } from './banner';
 
-export interface IPage404Props {
-}
-
-export function Page404 (props: IPage404Props) {
+export function Page404 () {
   return (
-    <>
-      <h1>Ooops, no treats here..</h1>
-      <section>
-        <span>slider image 1</span>
-        <span>slider image 2</span>
-        <span>slider image 3</span>
-        <span>slider image 4</span>
-        <span>slider image 5</span>
-      </section>
-      <Link to={'/'}>Back to Home</Link>
-    </>
+      <ImgBanner
+        hasButton={true}
+        buttonText='Back to Home'
+        buttonLink='/'
+        title='Ooops, no treats here..'
+        description={"This page does not exist."}
+        imgSrc='https://i.ibb.co/hRwLsFH/page404.webp'
+        shouldBeGrid={false} />
   );
 }
 
