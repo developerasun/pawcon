57c570dede47e60bfdd92f41f7a811499b4687f9
designerasun <asuntheawesome@gmail.com>
2022-03-17 01:38:51 +0900

    chore: update web3 apis

--- client/src/components/containers/C_apiUrl.tsx
+++ client/src/components/containers/C_apiUrl.tsx
@@ -30,3 +30,23 @@ export const API_DEV = {
     },
     signup : "http://localhost:3001/signup"
 }
+
+export const PROVIDER = { 
+    INFURA : 
+    {
+        MAINNET : "https://mainnet.infura.io/v3/a863bd289bd94db793914e7fb76dccba",
+        ROPSTEN : "https://ropsten.infura.io/v3/201092bdfa434e76ad85b29f00ea832e"
+    },
+    HARDHAT : "HTTP://127.0.0.1:8545", 
+}
+
+export const dummies = { 
+    account1 : { 
+        address : '0xEcAB21327B6EbA1FB0631Dc9bBc5863B6B2be3E4', 
+        privateKey : '0xa89591a01d7617197f5665a668da8f546f9632ba9c6c8beb7c1f57cb06a27e71', 
+    }, 
+    account2 : { 
+        address : '0x56012CaBddfF2bA8f3C36EBDe507eb8948c5CBfA', 
+        privateKey : '0xd1a90b633cd34ea4aaa8a5fe8f250a31801f0f4cdad12687e6674a8d0dd9e812', 
+    }
+} 
