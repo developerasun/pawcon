4e042f29770e19c3411e82b00b7a8c704026afd0
designerasun <asuntheawesome@gmail.com>
2022-03-17 23:07:35 +0900

    fix: nav dropdown deleted, button added, style fixed

--- client/src/components/subComponents/navbar.tsx
+++ client/src/components/subComponents/navbar.tsx
@@ -1,53 +1,17 @@
 import * as React from 'react'
 import './sass/css/navbar.css'
 import { Link } from 'react-router-dom'
-import { v4 as uuidV4 } from 'uuid'
-import { DropDownProps } from '../containers/C_props'
-
-const DropDown = ( {mainTitle, subTitle, routing}:DropDownProps ) => {
-    return (
-        <ul className='dropdown'>
-            <Link to={`${routing}`}>{mainTitle}</Link>
-            <ul className='dropdownSub deactive'>
-                {subTitle.map((item)=>{
-                    return <li key={uuidV4()}>{item}</li>
-                })}
-            </ul>
-        </ul>
-    )
-}
+import { Button } from './button'
 
 export function Navbar () {
     const [toggle, setToggle] = React.useState<boolean>(false)
     const handleClick = () => setToggle(!toggle)
     const handleToggle = () => setToggle(!toggle) // reset navbar once clicked
-
-    // useEffect to add dropdown effect
-    React.useEffect(()=>{
-        const navbarMenu = document.getElementById("menu") as HTMLElement
-        const dropdownSubs = document.getElementsByClassName("dropdownSub")
-        
-        navbarMenu.addEventListener("mouseenter", ()=>{
-            Array.from(dropdownSubs).map((item)=>{
-                item.classList.remove("deactive")
-                item.classList.add("active")
-                return ""
-            })
-        })
-
-        navbarMenu.addEventListener("mouseleave", ()=>{
-            Array.from(dropdownSubs).map((item)=>{
-                item.classList.remove("active")
-                item.classList.add("deactive")
-                return ""
-            })
-        })
-
-    }, [])
     
   return (
     <div id='navigation'>
         <Link to={'/'} className='logoContainer' > 
+            {/* change logo to the one with brand name: 'PawCon' */}
             <img src="https://i.ibb.co/2tfRH08/pawcon-logo.png" alt="pawcon pirate cat" id='logo'/>
         </Link>
         <ul className='buttons'>
@@ -63,13 +27,17 @@ export function Navbar () {
                 id='lists'
                 className={toggle ? 'unfold' : 'fold'}>
                 <li><Link to={'/'}>Main</Link></li>
-                <li><DropDown mainTitle="Create" subTitle={["Draw", "Pixelate", "Generate"]} routing="/create"/></li>
+                <li><Link to={'/create'}>Create</Link></li>
                 <li><Link to={'/gallery'}>Gallery</Link></li>
                 <li><Link to={'/shop'}>Shop</Link></li>
-                <li><DropDown mainTitle="Community" subTitle={["Feedback", "Chatting"]} routing="/community"/></li>
+                <li><Link to={'/community'}>Community</Link></li>
                 <li><Link to={'/about'}>About</Link></li>
                 <li><Link to={'/login'}>Login/SignUp</Link></li>
+                <li><Button 
+                        btnText='Get Started'
+                        url='/signup' /></li>
             </ul>
+
         </div>
     </div>
   );
