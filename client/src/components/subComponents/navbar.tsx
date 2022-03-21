337e76f178bad7e444ac5963533a6c871d58e7af
designerasun <asuntheawesome@gmail.com>
2022-03-21 17:56:36 +0900

    refactor: bar toggling, mobile media query added

--- client/src/components/subComponents/navbar.tsx
+++ client/src/components/subComponents/navbar.tsx
@@ -2,12 +2,31 @@ import * as React from 'react'
 import './sass/css/navbar.css'
 import { Link } from 'react-router-dom'
 import { Button } from './button'
+import { useAppSelector } from '../containers/redux/store.hooks'
+
+// $mobile : 576px;
+// $tablet : 768px; 
+// $desktop : 992px;
+type mobileBreakpoints = 576
+const mobileSize : mobileBreakpoints = 576
 
 export function Navbar () {
     const [toggle, setToggle] = React.useState<boolean>(false)
+    const isLogin = useAppSelector((state) => state.login.isLogin)
+    const isGoogleLogin = useAppSelector((state) => state.googleLogin.success)
+
     const handleClick = () => setToggle(!toggle)
-    const handleToggle = () => setToggle(!toggle) // reset navbar once clicked
+    const handleToggle = () => setToggle(false)
     
+    // detect window size for CSS media query
+    React.useEffect(() => {
+      window.addEventListener("resize", () => {
+        if (window.innerWidth > mobileSize) {
+            setToggle(false) //reset toggle state
+        }
+      }) 
+    }, []) // execute once
+
   return (
     <div id='navigation'>
         <Link to={'/'} className='logoContainer' > 
@@ -26,18 +45,17 @@ export function Navbar () {
             <ul
                 id='lists'
                 className={toggle ? 'unfold' : 'fold'}>
-                <li><Link to={'/'}>Main</Link></li>
-                <li><Link to={'/create'}>Create</Link></li>
-                <li><Link to={'/gallery'}>Gallery</Link></li>
-                <li><Link to={'/shop'}>Shop</Link></li>
-                <li><Link to={'/community'}>Community</Link></li>
-                <li><Link to={'/about'}>About</Link></li>
-                <li><Link to={'/login'}>Login/SignUp</Link></li>
-                <li><Button 
+                <li className='listsItem'><Link to={'/'}>Main</Link></li>
+                <li className='listsItem'><Link to={'/create'}>Create</Link></li>
+                <li className='listsItem'><Link to={'/gallery'}>Gallery</Link></li>
+                <li className='listsItem'><Link to={'/shop'}>Shop</Link></li>
+                <li className='listsItem'><Link to={'/community'}>Community</Link></li>
+                <li className='listsItem'><Link to={'/about'}>About</Link></li>
+                <li className='listsItem'><Link to={'/login'}>{!isLogin && !isGoogleLogin ? "Login" : "Profile"}</Link></li>
+                <li className='listsItem'><Button 
                         btnText='Get Started'
                         url='/signup' /></li>
             </ul>
-
         </div>
     </div>
   );
