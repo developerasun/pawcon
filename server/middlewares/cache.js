341a72df61b3026ece8af9cb1099ba5602718c5d
designerasun <asuntheawesome@gmail.com>
2022-03-22 14:58:38 +0900

    refactor: logger format changed, feat: add logger to cache middleware

--- server/middlewares/cache.js
+++ server/middlewares/cache.js
@@ -1,6 +1,11 @@
 // set server-side cache 
 // TO DO : check cache header
+const path = require('path')
+const logger = require('./logger')
+const fileName = path.basename(__filename)
+
 const setCache = (req, res, next) => {
+    logger(fileName)
     // aggressive cache for React index.html
     const cacheTime = 31536000 // cache stored for 1 years
     if (req.method === 'GET') {
--- server/middlewares/logger.js
+++ server/middlewares/logger.js
@@ -1,31 +1,20 @@
 const config = require('../config/config')
 
-// TO DO : use logOption object
-const logOption = {
-    fileOrigin : fileName, 
-    cookie : {
-        google : req.cookies[config.AUTH.GOOGLE.COOKIE.NAME],
-        jwt : req.cookies[config.AUTH.JSONWEBTOKEN.NAME]
-    }, 
-    passport : {
-        user : req.user, 
-        session : req.session
-    }, 
-    user : {
-        google : config.AUTH.GOOGLE.USER
-    }
-}
-
 // logger takes a custom parameter and wraps req,res middleware 
 const logger = (fileName) => {
     return (req, res, next) => {
-        console.log("=============================")
-        console.log(`logger coming from : ${fileName}`)
-        console.log(`google cookie : ${config.AUTH.GOOGLE.COOKIE.NAME}. jsonwebtoken cookie: ${config.AUTH.JSONWEBTOKEN.NAME}`)
-        console.log(`passport user check: ${req.user}`) // passport save user info into request.user(Express.user)
-        console.log(`checking passport session : ${req.session}`)
-        console.log(`google user check : ${config.AUTH.GOOGLE.USER}`)
-        console.log("=============================")
+        const googleUser = config.AUTH.GOOGLE.USER
+        const message = 'Does not exist'
+        // passport save user info into request.user(Express.user)
+        console.log(
+            ` 
+            =============================
+            logger coming from : ${fileName}
+            passport user check: ${req.user ? req.user : message }
+            checking passport session : ${req.session ? req.session : message }
+            google user check : ${ googleUser ? googleUser : message}
+            =============================
+            `)
         next() // move to next middleware
     }   
 }
