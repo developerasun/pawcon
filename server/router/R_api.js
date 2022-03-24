2c52d41d20dd7e91ef564e95a00a0f329e497df4
designerasun <asuntheawesome@gmail.com>
2022-03-24 23:54:01 +0900

    chore: minor updates

--- server/router/R_api.js
+++ server/router/R_api.js
@@ -8,5 +8,6 @@ const apiRouter = require('express').Router()
 // base route : /apis
 apiRouter.get('/users/google', apiController.getGoogleOauthUser)
 apiRouter.get('/artworks/:page', apiController.getArtworks) // set req.params.page
+apiRouter.get('/feedbacks', apiController.getFeedbacks)
 
 module.exports = apiRouter
--- server/router/R_feedback.js
+++ server/router/R_feedback.js
@@ -5,9 +5,8 @@ const feedbackPath = path.join(__dirname, '..', 'controller', 'C_feedback'.conca
 const feedbackController = require(feedbackPath)
 const feedbackRouter = require('express').Router()
 
-// TO DO : add api route for feedback
-// feedback requests coming from '/community/feedback' route
-feedbackRouter.post('/feedback:page', feedbackController.feedback_post)
-// feedbackRouter.delete('/community', feedbackController.feedback_delete)
+feedbackRouter.post('/', feedbackController.feedback_post)
+// TO DO : add DELETE request
+// feedbackRouter.delete('/', feedbackController.feedback_delete)
 
 module.exports = feedbackRouter
