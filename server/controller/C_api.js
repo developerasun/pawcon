e170250c4adffeb49de9291e640e634d9b54640a
designerasun <asuntheawesome@gmail.com>
2022-03-24 23:53:33 +0900

    feat: add editor js api handler

--- server/controller/C_api.js
+++ server/controller/C_api.js
@@ -5,16 +5,19 @@ const pawconFeedback = require('../model/post')
 const path = require('path')
 const fs = require('fs')
 
+const OK = 200 // status code 200
+const UNAUTHORIZED = 401 
+
 // send user to client if exists
 const getGoogleOauthUser = async (req, res) => {
     try {
         // not logged out and user info saved in googleUser variable
         if (config.AUTH.GOOGLE.USER !== null) {
             const user = await pawconGoogleUser.findOne({googleId : config.AUTH.GOOGLE.USER.googleId})
-            res.status(200).json({ success : true, username : user.username, thumnail : user.thumnail })
+            res.status(OK).json({ success : true, username : user.username, thumnail : user.thumnail })
         } else { 
             console.log("no google user in req object")
-            res.status(401).json( { success : false, user : null } )
+            res.status(UNAUTHORIZED).json( { success : false, user : null } )
         }
     } catch(err) {
         console.log(err)
@@ -25,17 +28,27 @@ const getGoogleOauthUser = async (req, res) => {
 const getArtworks = (req, res) => {
     const artworks = fs.readFileSync(path.join(__dirname, '..', 'apis', 'artworks', 'json',`artwork${req.params.page}.json`))
     res.set('Content-Type', 'application/json')
-    res.status(200).send(artworks.toString()) // send string data and set HTTP 200 OK success status
+    res.status(OK).send(artworks.toString()) // send string data and set HTTP 200 OK success status
 }
 
-// TO DO : API controller for feedbacks
-// send latest 10 posts at a time
+// TO DO : send latest 5 posts dynamically at a time
 const getFeedbacks = async (req, res) => {
-    const posts = await pawconFeedback.find() // get all posts
-    console.log(posts)
+    try {
+    const fivePosts =
+        await pawconFeedback
+            .find()
+            .sort({"created_At":"desc"}) // order : latest
+            .limit(5) // number of feedbacks per request: 5
+        if (typeof fivePosts !== undefined || null) {
+            res.status(OK).json({posts : fivePosts})
+        }
+    }catch(err) {
+        console.log(err)
+    }
 }
 
 module.exports = { 
     getGoogleOauthUser, 
-    getArtworks
+    getArtworks,
+    getFeedbacks
 }
