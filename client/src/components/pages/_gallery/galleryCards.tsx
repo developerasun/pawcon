d0af8b2a7fd73a5525c8167918657defbaf5ab35
designerasun <asuntheawesome@gmail.com>
2022-03-30 22:15:09 +0900

    feat: redux state added for fetched artwork

--- client/src/components/pages/_gallery/galleryCards.tsx
+++ client/src/components/pages/_gallery/galleryCards.tsx
@@ -2,9 +2,25 @@ import * as React from 'react';
 import './sass/css/gallery.css'
 import { Link } from 'react-router-dom';
 import { GalleryCardsProps } from '../../containers/C_props'
+import { useAppDispatch } from '../../containers/redux/store.hooks';
+import { fetchArtwork } from '../../containers/redux/actionCreators';
 
 export function GalleryCards ( { title, description, author, date, image }:  GalleryCardsProps) {
-  
+  const oneArtwork = {
+    title, 
+    description, 
+    author, 
+    date,
+    image
+  }
+  const dispatch = useAppDispatch()
+  // add Redux logic to deliver props to renderDetails component
+
+  const handleDetails = () => {
+    console.log("cliekd")
+    // store every details to Redux store to render it in lower comp
+    dispatch(fetchArtwork(oneArtwork))
+  }
   return (
     <div className='card' >
       <img 
@@ -18,7 +34,7 @@ export function GalleryCards ( { title, description, author, date, image }:  Gal
         <h3 className='title'>{title}</h3>
         <span className='author'>{author}</span>
         <p className='description'>{description}</p>
-        <Link to={`/details/${title}`} > <u><i>See Details</i></u> </Link>
+        <Link to={`/details/${title}`} > <u><i onClick={handleDetails}>See Details</i></u> </Link>
       </div>
     </div>
   );
